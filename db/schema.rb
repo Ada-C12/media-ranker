8cf7be19e4a5e20c8a305f9198cb47e0871c8499
Janice Huang <janicehuang@gmail.com>
2019-10-14 22:18:56 -0700

    add vote model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_043611) do
+ActiveRecord::Schema.define(version: 2019_10_15_051751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2019_10_15_043611) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.date "date_joined"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
