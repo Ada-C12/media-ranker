1d1bcba28ab0a3169f4423658e4db7a8eca8db6d
Janice Huang <janicehuang@gmail.com>
2019-10-14 22:27:49 -0700

    change name of 'type' column in works to 'category' bc interfering with db

---
+++ db/migrate/20191015052600_change_name_of_type_column_in_works.rb
@@ -0,0 +1,5 @@
+class ChangeNameOfTypeColumnInWorks < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :works, :type, :category
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_052123) do
+ActiveRecord::Schema.define(version: 2019_10_15_052600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_052123) do
   end
 
   create_table "works", force: :cascade do |t|
-    t.string "type"
+    t.string "category"
     t.string "title"
     t.string "creator"
     t.integer "release_date"
