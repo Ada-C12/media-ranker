03d9e0fd0fa85d57765f62d86d870add32269c33
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-15 16:15:29 -0700

    Wrote index page for homepage and made model methods in Work.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,13 @@
 class Work < ApplicationRecord
+
+  def self.spotlight
+    works = Work.all
+    return works.sample
+  end
+
+  def self.top_ten(category)
+    works = Work.where(category: category)
+    return works.sample(10)
+
+  end
 end
