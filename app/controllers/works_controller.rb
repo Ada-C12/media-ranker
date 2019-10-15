d0b11a01d1797ffc24e063d0b3bf1a1ba89ab860
Mira <calopter@gmail.com>
2019-10-14 19:37:05 -0700

    main page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,11 @@ class WorksController < ApplicationController
     @books = Work.where(category: "book")
     @movies = Work.where(category: "movies")
   end
+  
+  def main
+    @spotlight = Work.spotlight
+    @top_movies = Work.top_ten "movie"
+    @top_books = Work.top_ten "book"
+    @top_albums = Work.top_ten "album"
+  end
 end
