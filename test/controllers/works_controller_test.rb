96790e9fcd3b4e098f294e55ac34ff60203f6d4b
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-15 15:49:46 -0700

    Added some test pseudocode

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -5,11 +5,50 @@ describe WorksController do
   #   value(1+1).must_equal 2
   # end
   
-  it "can get a work" do 
+  describe "index" do
+    it "can get a list of all the works" do 
+      get works_path
+      
+      must_respond_with :success
+    end
   end
+
   # show
+  describe "show" do
+    it "can get a work" do
+      work = Work.find(1)
+
+      get work_path(work.id)
+
+      must_redirect_to work_path(work.id)
+    end
+  end
+
   # create
+  describe "create" do
+    it "can create a new work" do
+      params = {
+        category: "album",
+        title: "New work",
+        creator: "New creator",
+        publication_year: 2000,
+        description: "Cool new work"
+      }
+      expect{ Work.create(params) }.must_differ "Work.count", 1
+
+      work = Work.find_by(title: "New work", category: "album")
+      expect(work.creator).must_equal "New creator"
+      expect(work.publication_year).must_equal 2000
+      expect(work.description).must_equal "Cool new work"
+      
+    end
+  end
+
   # edit
+  describe "edit" do
+  end
+
   # destroy
-  
+  describe "destroy" do
+  end
 end
