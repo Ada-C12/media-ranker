07fbbe43d080ace32cb4188b39fcaea83610f35f
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-10-14 21:05:17 -0700

    added seed files, nav bar

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,78 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all 
+  end
+  
+  def show
+    @work = Work.find_by(id: params[:id])
+    
+    if @work.nil?
+      # Might not be the correct redirect??
+      # The page you were looking for doesn't exist. You may have mistyped the address or the page may have moved.
+      head :not_found
+      return
+    end
+  end
+  
+  def new
+    @work = Work.new
+  end
+  
+  def create
+    @work = Work.new(work_params)
+    
+    if @work.save  
+      redirect_to work_path(@work.id)  
+      return
+    else 
+      render :new  
+      return
+    end
+  end
+  
+  def edit
+    @work = Work.find_by(id: params[:id])
+    
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def update
+    @work = Work.find_by(id: params[:id])
+    
+    if @work.nil?
+      head :not_found
+      return
+    end
+    
+    if @work.update(work_params)
+      redirect_to work_path(@work.id)  
+      return
+    else  
+      render :edit  
+      return
+    end
+  end
+  
+  def destroy
+    @work = Work.find_by(id: params[:id])
+    
+    if @work.nil?
+      head :not_found
+      return
+    end
+    
+    @work.destroy
+    
+    redirect_to works_path
+    return
+  end
+  
+  private
+  
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
