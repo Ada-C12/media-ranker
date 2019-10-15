75f7f8cc04fb496e518cf1f5c2a264507cc9e814
Hallie <idhallie@gmail.com>
2019-10-15 16:17:37 -0700

    Added category_sort class method and working on formatting for works#index.

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,22 @@ class Work < ApplicationRecord
   has_many :votes
   
   validates :title, presence: true, uniqueness: { scope: :category }
+  
+  def self.media_types
+    media_types = {}
+    
+    works = Work.all
+    works.each do |item|
+      type = item.category
+      if media_types[:type].nil?
+        media_types[:type] = true
+      end
+      return media_types
+    end
+  end
+  
+  def self.media_sort(category)
+    sorted_media = Work.where(category: category)
+    return sorted_media
+  end
 end
