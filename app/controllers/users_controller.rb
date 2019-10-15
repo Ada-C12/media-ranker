3a0139e8606f1ccd1fdd6204f0029d7bf384b5ef
Julia Bouvier <juliabouv@gmail.com>
2019-10-15 16:47:57 -0700

    Table columns created and model relationships created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+  def login_form
+    @user = User.new
+  end
+  
+  def login
+    username = params[:user][:username]
+    @user = User.find_by(username: username)
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in as returning user #{username}"
+    else
+      @user = User.new(username: username)
+      @user.save
+
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
+    redirect_to root_path
+  end
+  
+  def current
+    @current_user = User.find_by(id: session[:user_id])
+  end
+  
+  def logout
+    unless session[:user_id]
+      flash[:error] = "No user is logged in"
+    else
+      session[:user_id] = nil
+      flash[:success] = "Successfully logged out"
+    end
+    redirect_to root_path
+  end
 end
