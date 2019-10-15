af85112bba3a86ef504328fbb151c8f693525865
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-14 20:04:39 -0700

    Created all controller actions for Works and added a partial view form

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,4 +11,64 @@ class WorksController < ApplicationController
       return
     end
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to work_path(@work)
+      return
+    else
+      render "new"
+      return
+    end
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path
+      return
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end
+
+    if @work.update(work_params)
+      redirect_to work_path(@work)
+      return
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path
+      return
+    end
+
+    @work.destroy
+    redirect_to root_path
+    return
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
