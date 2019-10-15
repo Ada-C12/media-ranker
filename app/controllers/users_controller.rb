6e46da17bf27b46e7a0ff04964ee8eafab7d3bad
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-14 22:57:54 -0700

    user#show now works, and user#index shows correct table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
   
   def index
+    # works but not tested
     @users = User.all.order(:id)
   end
   
   def login
+    # works but not tested
     @user = User.new
   end
   
   def create
+    # works but not tested
+    
     name_input = params[:user][:name]
     @user = User.new(name: name_input, votes_casted: 0)
     
@@ -29,6 +33,11 @@ class UsersController < ApplicationController
   end
   
   def show
-    @existing_user = params[:id]
+    # works but not tested
+    @user = User.find_by(id: params[:id])
+    unless @user
+      head :not_found
+    end
   end
+  
 end
