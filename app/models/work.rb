e757fa4939e780b93bd8a6d416a8a1f08eb37511
Paige Davenport <davenport.paige@outlook.com>
2019-10-15 15:45:03 -0700

    Added Homepages controller view and CRUD actions for Works Controller.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,4 @@
 class Work < ApplicationRecord
   validates :category, inclusion: { in: ["album", "book", "movie"] }
-  validates :title, presence: true, uniqueness: {scope: :category}
+  validates :title, presence: true, uniqueness: { scope: :category }
 end
