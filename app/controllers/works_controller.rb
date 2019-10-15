72d17afe6f3d4f2bbab9c5f7f384f80b3560d865
Alice <alicesunhi@gmail.com>
2019-10-15 15:44:02 -0700

    built index page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,8 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all
+    @albums = Work.where(category: "album")
+    @movies = Work.where(category: "movie")
+    @books = Work.where(category: "book")
   end
   
   def show
