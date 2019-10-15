03ff9a092dd8e651ee6c791809a56392f5f3f962
Eve Le <nguyenthao.lee@gmail.com>
2019-10-15 16:40:36 -0700

    Implement seeds.rb to read in data from media-seeds.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+MEDIA_FILE = Rails.root.join('db', 'media-seeds.csv')
+puts "Loading raw works data from #{MEDIA_FILE}"
+
+work_failures = []
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
+  work = Work.new
+  work.title = row['title']
+  work.category = row['category']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
+
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
