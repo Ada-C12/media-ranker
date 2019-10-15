328aa49f001e0429e225f923f01fe7e2177fc300
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-15 15:38:26 -0700

    Added delete link.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -60,7 +60,7 @@ class WorksController < ApplicationController
 
   def destroy
     work_id = params[:id]
-    @work = find_by(id: work_id)
+    @work = Work.find_by(id: work_id)
   
     if @work.nil?
        head :not_found
