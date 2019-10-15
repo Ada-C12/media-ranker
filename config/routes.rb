9471574ea91ea2cc6b2098bb8f2a66cfb5361d61
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-15 15:10:07 -0700

    Oops, overlooked controller for works. Added.

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   # Set the homepage route
   root 'homepages#index'
   
-  # resources :works 
-  # resources :users
+  resources :works 
+  resources :users
     
-  # get "/login", to: "users#login_form", as: "login"
-  # post "/login", to: "users#login"
-  # post "/logout", to: "users#logout", as: "logout"
-  # get "/users/current", to: "users#current", as: "current_user" 
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user" 
 
 end
