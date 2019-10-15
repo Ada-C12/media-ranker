2e6feec31a69f58e9df853511772d47f07d908bd
Paige Davenport <davenport.paige@outlook.com>
2019-10-15 09:32:35 -0700

    Modified seed configuration.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+WORK_FILE = Rails.root.join('db', 'seed_data', 'media-seeds.csv')
+
+work_failures = []
+
+CSV.foreach(WORK_FILE, :headers => true) do |row|
+  new_media = Work.new
+  new_media.category = row["category"]
+  new_media.title = row["title"]
+  new_media.creator = row["creator"]
+  new_media.publication_year = row["publication_year"]
+  new_media.description = row["description"]
+  successful = new_media.save
+  
+  if successful
+    puts "Created new media: #{new_media.inspect}"
+  else
+    work_failures << new_media
+    puts "Failed to save media: #{new_media.inspect}"
+  end
+end
+
+puts "#{Work.count} new media records added successfully"
+puts "#{work_failures.length} media records failed to save"
