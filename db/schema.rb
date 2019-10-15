913ad2e5d4feffbb2ab518d91e676242f5579886
Janice Huang <janicehuang@gmail.com>
2019-10-14 21:38:27 -0700

    add description column to works model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_042459) do
+ActiveRecord::Schema.define(version: 2019_10_15_043611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_042459) do
     t.integer "release_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
