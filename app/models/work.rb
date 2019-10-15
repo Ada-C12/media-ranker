c84263234f6302abf691803ba3ae509fafa437dd
Janice Huang <janicehuang@gmail.com>
2019-10-15 16:34:07 -0700

    fix type to category, db reset, get top ten movies list populating

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,7 @@
 class Work < ApplicationRecord
   has_many :votes#, dependent: :nullify
   validates :title, presence: true
-  validates :type, presence: true
+  validates :category, presence: true
   #validates :title, presence: true
 
 end
