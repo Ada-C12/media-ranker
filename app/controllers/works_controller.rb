d385f912aa96c66fccf67a918818f6ce1bb867d5
Dominique <dom_taylor@icloud.com>
2019-10-15 16:17:09 -0700

    Wave 1 - allow users to add new works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,22 @@ class WorksController < ApplicationController
     @albums= Work.where(category: "album")
     @books = Work.where(category: "book")
   end
+
+  def new
+    @work = Work.new
+  end 
+
+  def create
+    @work = Work.new(category: params[:work][:category], title: params[:work][:title], creator: params[:work][:creator], publication_year: params[:work][:publication_year], description: params[:work][:description] )
+    
+    if @work.save 
+      flash[:sucess] = "Work added successfully"
+      redirect_to root_path
+      return
+    else
+      flash.now[:failure] = "Work did not add successfully"
+      render :new 
+      return 
+    end
+  end 
 end
