ce9620e1abb232819d615e0f6cc7d128ef89b800
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-15 00:11:05 -0700

    Works#index can now show all movies, upvote button broken though

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_063417) do
+ActiveRecord::Schema.define(version: 2019_10_15_070400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 2019_10_15_063417) do
     t.string "creator"
     t.integer "published_year"
     t.string "description"
-    t.integer "votes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "votes_earned"
   end
 
   create_table "users", force: :cascade do |t|
