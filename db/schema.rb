0ab59f9737bc22e47b86fbeb52b851c83fae1b8a
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-10-15 16:56:50 -0700

    fixes schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_232856) do
+ActiveRecord::Schema.define(version: 2019_10_15_234746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 2019_10_15_232856) do
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "work"
-    t.integer "user"
     t.bigint "work_id"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
