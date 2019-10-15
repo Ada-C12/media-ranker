e02bc3b365edeedd157a1c9f202a05ea08d3882e
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-15 16:05:34 -0700

    Created seed script and seed db and Work model and schema

---
+++ db/migrate/20191015225551_add_columns_to_work_model.rb
@@ -0,0 +1,9 @@
+class AddColumnsToWorkModel < ActiveRecord::Migration[5.2]
+  def change
+    add_column(:works, :category, :string)
+    add_column(:works, :title, :string)
+    add_column(:works, :creator, :string)
+    add_column(:works, :publication_year, :string)
+    add_column(:works, :description, :string)
+  end
+end
