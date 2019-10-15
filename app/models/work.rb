66ae9f2c78c80452f9d8364ae91cecaa202cbcc9
Janice Huang <janicehuang@gmail.com>
2019-10-14 22:16:03 -0700

    add belongs-to and has-many in models, add some validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,6 @@
 class Work < ApplicationRecord
+  has_many :votes#, dependent: :nullify
+  validates :title, presence: true
+  validates :type, presence: true
+  #validates :title, presence: true
 end
