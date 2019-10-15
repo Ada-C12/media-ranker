041b55c3e8427fc4716256b9f694e541a37bf7ea
Ga-Young Jin <gyjinn@gmail.com>
2019-10-15 16:01:53 -0700

    added a homepage controller and root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
-  get 'votes/index'
-  get 'votes/show'
-  get 'votes/new'
-  get 'votes/create'
-  get 'votes/edit'
-  get 'votes/update'
-  get 'votes/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
+  root 'homepages#index'
+  resources :homepages
   resources :works
   resources :votes
   resources :users
