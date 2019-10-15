6e69c7229cef746842634697b5fa2f370b2fbe14
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-15 14:25:36 -0700

    Created login and log out function. Added to application.html.erb. Wrote methods #login_form, #login, #current, #logout in Users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    name = params[:user][:name]
+    user = User.find_by(name: name)
+
+    if user 
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{name}."
+    else
+      user = User.create(name: name)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{name}."
+    end
+    redirect_to root_path
+  end
+
+  def current
+    @current_user = User.find_by(id: session[:user_id])
+    unless @current_user
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:logout] = "You are now logged out."
+    redirect_to root_path
+  end
+
 end
