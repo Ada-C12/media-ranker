014edf80076ddadea21000bb5e5a144683583ecb
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-14 22:49:20 -0700

    can validate user name, but routing to show page afterwards is bad

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,19 +13,22 @@ class UsersController < ApplicationController
     @user = User.new(name: name_input, votes_casted: 0)
     
     if @user.valid?
-      # new user
       @user.save
+      redirect_to user_path(@user.id)
       
     else 
       @existing_user = User.find_by(name: name_input)
       if @existing_user
-        # user already exists
-        redirect_to user_path(@existing_user)
+        @user = @existing_user
+        redirect_to user_path(@user.id)
       else
         # bogus input 
         render action: "login"
       end
     end
-    
+  end
+  
+  def show
+    @existing_user = params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   
   root "homepages#index"
   
-  resources :users, only: [:index, :create, :show]
+  resources :users, only: [:index, :create]
   get '/users/login', to: "users#login", as: "login"
+  get '/users/:id', to: "users#show", as: "user"
   
 end
