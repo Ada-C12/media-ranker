a147be9fa685635ae0fd0ab65b9791a1ee2b27f7
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-15 14:30:12 -0700

    Created login_form, login, current User controllers actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root "homepages#index"
 
   resources :works
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+  get "/users", to: "users#index", as: "users"
 end
