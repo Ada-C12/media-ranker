204b836a380e79bd1b07a72c094f5ec8ed78ded9
Ga-Young Jin <gyjinn@gmail.com>
2019-10-15 16:42:12 -0700

    added a work show page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,9 +1,18 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all
+    @all_albums = Work.all_albums
+    @all_books = Work.all_books
+    @all_movies = Work.all_movies
   end
   
   def show
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+    
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
   
   def new
--- app/models/work.rb
+++ app/models/work.rb
@@ -6,4 +6,35 @@ class Work < ApplicationRecord
   validates :creator, presence: true
   validates :publication_year, presence: true, numericality: {only_integer: true, greater_than: 1000}
   validates :description, presence: true
+  
+  def self.all_albums
+    albums = []
+    Work.all.each do |work|
+      if work.category == "album"
+        albums << work
+      end
+    end
+    return albums
+  end
+  
+  def self.all_books
+    books = []
+    Work.all.each do |work|
+      if work.category == "book"
+        books << work
+      end
+    end
+    return books
+  end
+  
+  def self.all_movies
+    movies = []
+    Work.all.each do |work|
+      if work.category == "movie"
+        movies << work
+      end
+    end
+    return movies
+  end
+  
 end
