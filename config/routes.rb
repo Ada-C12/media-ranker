cd7afad35af02c8151774a7bf34f5b7c13779a6c
Ga-Young Jin <gyjinn@gmail.com>
2019-10-15 15:53:15 -0700

    added work, user, vote controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'votes/index'
+  get 'votes/show'
+  get 'votes/new'
+  get 'votes/create'
+  get 'votes/edit'
+  get 'votes/update'
+  get 'votes/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+  resources :works
+  resources :votes
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
