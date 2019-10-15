0b15190e4fc8ad809644c728fe9a3ca8b122958e
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-14 18:01:29 -0700

    Implemented top_ten and top_media Work model methods

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,12 @@
 class Work < ApplicationRecord
+  has_many :votes
+  validates :title, presence: true, uniqueness: true
+
+  def self.top_ten(category)
+    return sorted_works = Work.where(category: category).sort_by { |work| work.votes.count }.reverse!.first(10)
+  end
+
+  def self.top_media
+    return top_work = self.all.max_by { |work| work.votes.count }
+  end
 end
