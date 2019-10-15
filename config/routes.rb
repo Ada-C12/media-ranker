a7890081bd62b4eca6487e2c48a9f1f437e356fc
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-10-14 20:05:07 -0700

    added homepage and works routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homepages#index'
+  get '/homepages', to: 'homepages#index', as: 'homepages'
+  
+  resources :works, except: [:update]
+  patch '/works/:id', to: 'works#update'
 end
