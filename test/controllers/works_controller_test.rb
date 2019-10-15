0100fede5fb18160d150ba292fb80a3f3044cfe3
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-14 19:21:22 -0700

    Seeded db, generated models and controllers

---
+++ test/controllers/works_controller_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+describe WorksController do
+  it "must get users" do
+    get works_users_url
+    must_respond_with :success
+  end
+
+  it "must get votes" do
+    get works_votes_url
+    must_respond_with :success
+  end
+
+end
