0ddac239986655841f4765b4f3f156c8f7333e4a
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-15 15:19:00 -0700

    experiment, deleted model and ctrller for Movies.  Gonna use just Works for all 3 from now on

--- app/controllers/movies_controller.rb
+++
@@ -1,6 +0,0 @@
-class MoviesController < ApplicationController
-  
-  ### All media are indexed on same page.  Go to Works_controller for that !!
-  
-  
-end
--- test/controllers/movies_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe MoviesController do
-  it "must get index" do
-    get movies_index_url
-    must_respond_with :success
-  end
-
-end
