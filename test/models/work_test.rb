69c6743766954f33b21381637df9c8bf3a025e5f
Janice Huang <janicehuang@gmail.com>
2019-10-15 21:48:45 -0700

    start writing Work model tests, add minitest requires to testhelper

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,22 +2,27 @@ require "test_helper"
 
 describe Work do
   describe 'validations' do
+    it 'is valid when title and category are present, and release date is an integer' do
+      @work = works(:leithauser)
+      expect(@work.valid?).must_equal true
+    end
+
     it 'is invalid without a title' do
-    end
-  end
-end
-
-
-  describe 'relations' do
-    it "has an author" do
-      book = books(:poodr)
-      book.author.must_equal authors(:metz)
+      @work.title = nil
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title
     end
 
-    it "can set the author" do
-      book = Book.new(title: "test book")
-      book.author = authors(:metz)
-      book.author_id.must_equal authors(:metz).id
+    it 'is invalid without a category' do
+      @work.category = nil
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :category
+    end
+
+    it 'is invalid if the release date is not an integer' do
+    @work.release_date = 'abc'
+    expect(@work.valid?).must_equal false
+    expect(@work.errors.messages).must_include :release_date
     end
   end
 end
