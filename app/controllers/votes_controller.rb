b4ebb422cd0e5f03f136bb740b5405d79debfdc5
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-10-16 00:02:34 -0700

    added votes logic to most views. added upvote function. added uniqueness restrictions to vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,29 @@
 class VotesController < ApplicationController
   def upvote
     user = User.find_by(id: session[:user_id])
+    
     if user.nil?
-      flash.now[:warning] = "A problem occurred: You must log in to do that"
+      flash[:warning] = "A problem occurred: You must log in to do that"
+      redirect_back(fallback_location: root_path)
       return
     end
     
     work = Work.find_by(id: params[:work_id])
+    
     if work.nil?
       head :not_found
+      return
+    end
+    
+    @vote = Vote.new(work_id: work.id, user_id: user.id)
+    
+    if @vote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_back(fallback_location: root_path)
+      return
+    else
+      flash[:warning] = "A problem occurred: Could not upvote"
+      redirect_back(fallback_location: root_path)
       return
     end
   end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,8 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
   
+  validates_uniqueness_of :user_id, scope: :work_id, message: "has already voted for this work" 
+  
   # MIGHT NOT BE NECESSARY
   # validates :user_id, presence: true
   # validates :work_id, presence: true
