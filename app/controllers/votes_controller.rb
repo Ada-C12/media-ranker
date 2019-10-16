c8942d6794abb46d244f62c079c609a330489f8d
Janice Huang <janicehuang@gmail.com>
2019-10-16 16:17:47 -0700

    add vote buttons to works index page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,10 @@ class VotesController < ApplicationController
       flash[:error] = "Sorry, you may only vote once for this work!"
     else
     vote = Vote.create(date: Date.today)
+    work = Work.find(params[:work_id])
+    user = User.find(session[:user_id])
+    work.votes << vote
+    user.votes << vote
     end
   end
 end
