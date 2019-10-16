15baf5fdd826d03987f46f36621a3715ad38be9e
Eve Le <nguyenthao.lee@gmail.com>
2019-10-16 11:39:17 -0700

    Add implementation for top_works_sorted_by_category and highest_rated_work methods to Work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -7,4 +7,16 @@ class Work < ApplicationRecord
   def self.works_by_category(category)
     return Work.where(category: category)
   end
+
+  def self.top_works_sorted_by_category(category)
+    sorted_works = Work.works_by_category(category).sort {|a,b| b.votes.length <=> a.votes.length }  
+    if sorted_works.length > 10
+      return sorted_works.slice(0...10)
+    end
+    return sorted_works
+  end
+
+  def self.highest_rated_work
+    return Work.all.max_by{ |work| work.votes.length }
+  end
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -6,28 +6,36 @@ describe Work do
       expected_movies = Work.where(category: "movie")
       expected_books = Work.where(category: "book")
       expected_albums = Work.where(category: "album")
-
+      
       existing_movies = Work.works_by_category("movie")
       existing_books = Work.works_by_category("book")
       existing_albums = Work.works_by_category("album")
-
+      
       expect(existing_movies.length).must_equal expected_movies.length
       expect(existing_movies.sample.category).must_equal expected_movies.sample.category
-
+      
       expect(existing_books.length).must_equal expected_books.length
       expect(existing_books.sample.category).must_equal expected_books.sample.category
-
+      
       expect(existing_albums.length).must_equal expected_albums.length
       expect(existing_albums.sample.category).must_equal expected_albums.sample.category
     end
-
+    
     it "returns empty array if there's no object of provided category found" do
       Work.destroy_all
       expect(Work.count).must_equal 0
-
+      
       expect(Work.works_by_category("movie")).must_equal []
       expect(Work.works_by_category("book")).must_equal []
       expect(Work.works_by_category("album")).must_equal []
     end
+  end
+  
+  describe "top_works_sorted_by_category" do  
+    #Please don't forget to write test when Vote is all set up
+  end
+
+  describe "highest_rated_work" do  
+    #Please don't forget to write test when Vote is all set up
   end
 end
