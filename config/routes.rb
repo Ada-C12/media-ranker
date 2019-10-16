3cce52d31a0a45ef9cde8428e8f4eb8995d048d3
geli-gel <angele.zam@gmail.com>
2019-10-15 23:09:19 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: 'works#top_works'
+  root to: 'works#top_ten'
   
+  get '/works/top_ten', to: 'works#top_ten', as: 'top_ten_works'
+
   resources :works do
-    resources :votes, only: [:index]
+    resources :votes, only: [:index, :post]
   end
   
   # resources :users do
