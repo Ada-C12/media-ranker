393304eadddf892f14a0956a571b4dc9fc98558d
Eve Le <nguyenthao.lee@gmail.com>
2019-10-16 01:06:02 -0700

    Add view and implementation for works#show action

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,12 @@ class WorksController < ApplicationController
     @movies = Work.works_by_category("movie")
     @albums = Work.works_by_category("album")
   end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
 end
