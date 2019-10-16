131140fe0e38062a0b657f5677957ff06e1844cc
Ga-Young Jin <gyjinn@gmail.com>
2019-10-16 16:13:57 -0700

    added partials&controller filters for work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,19 +1,14 @@
 class WorksController < ApplicationController
+  before_action :find_work, only: [:show, :edit, :update, :destroy]
+  before_action :missing_work, only: [:show, :edit]
+  
   def index
     @all_albums = Work.all_albums
     @all_books = Work.all_books
     @all_movies = Work.all_movies
   end
   
-  def show
-    work_id = params[:id]
-    @work = Work.find_by(id: work_id)
-    
-    if @work.nil?
-      head :not_found
-      return
-    end
-  end
+  def show ; end
   
   def new
     @work = Work.new
@@ -31,18 +26,47 @@ class WorksController < ApplicationController
     end
   end
   
-  def edit
-  end
+  def edit ; end
   
   def update
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      redirect_to work_path(@work.id)
+      return
+    else 
+      render :edit
+      return
+    end
   end
   
   def destroy
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.destroy
+      redirect_to works_path
+      return
+    else
+      render :show
+    end
   end
   
   private
   
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+  
+  def find_work
+    @work = Work.find_by_id(params[:id])
+  end
+  
+  def missing_work
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 end
