26f9ab9a476023237cf5c6cc7923b1c1fd830811
Janice Huang <janicehuang@gmail.com>
2019-10-15 17:01:59 -0700

    add index page for works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,15 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+
+  
 end
