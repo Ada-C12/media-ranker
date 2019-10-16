214ddeb817c3d2e4347d5ec644a657525f81e2fb
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-15 21:34:11 -0700

    attempting User model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,5 +2,39 @@ require "test_helper"
 
 describe User do
   
+  let (:user1) { users(:user1) }
+  
+  describe "RELATIONS" do
+    it "can have many votes" do
+      ### NEED Votes Model
+    end
+    
+    it "userObj.votes exists and can return Vote objs" do
+      
+    end
+  end
+  
+  
+  describe "VALIDATIONS" do
+    it "Can create User obj, given good inputs" do
+      assert(user1.valid?)
+    end
+    
+    it "Won't create User obj, given bogus inputs" do
+      refute()
+      # expect (@test_object.erors.messages).must_include :attrib_name
+      # assert (@test_object.errors.messages.attrib_name == "expected error string")
+    end
+  end
+  
+  
+  
+  describe "CUSTOM METHOD #1" do
+    it "nominal case" do
+    end
+    
+    it "edge case" do
+    end
+  end
   
 end
