9869d9c6fb90ac63d975da0380f62f947b79b382
Eve Le <nguyenthao.lee@gmail.com>
2019-10-16 01:25:52 -0700

    Add view and implementation for works#create, update application layout to show flash message

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,7 +4,7 @@ class WorksController < ApplicationController
     @movies = Work.works_by_category("movie")
     @albums = Work.works_by_category("album")
   end
-
+  
   def show
     @work = Work.find_by(id: params[:id])
     if @work.nil?
@@ -12,8 +12,26 @@ class WorksController < ApplicationController
       return
     end
   end
-
+  
   def new
     @work = Work.new
+  end
+  
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      flash[:success] = "New work created!"
+      redirect_to work_path(Work.find_by(title: @work.title, category: @work.category))
+      return
+    end
+    
+    flash.now[:error] = "Failed to create work!"
+    render :new
+  end
+  
+  private
+  
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 end
