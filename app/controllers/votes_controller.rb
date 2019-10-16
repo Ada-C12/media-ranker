24ad271eda91dd3a97b5e578b9165581db6df763
Brianna Kemp <kemp.bri@gmail.com>
2019-10-16 16:47:06 -0700

    Added standard methods to users and votes controllers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,72 @@
+# To make a vote, we need one vote and one user
+
 class VotesController < ApplicationController
+  
+  def index
+    @votes = Vote.all
+  end
+  
+  def show
+    vote_id = params[:id].to_i
+    @vote = Vote.find_by(id: vote_id)
+    if @vote.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def new
+    @vote = Vote.new
+  end
+  
+  def create
+    # use params returned from work model
+    @vote = Vote.new( vote_params )
+    if @vote.save
+      redirect_to vote_path(@vote.id)
+    else
+      render new_vote_path
+    end
+  end
+  
+  def edit
+    @vote = Vote.find_by(id: params[:id] )
+    if @vote.nil?
+      redirect_to votes_path
+      return
+    end
+  end
+  
+  def update
+    @vote = Vote.find_by(id: params[:id] )
+    if @vote.nil?
+      head :not_found
+      return
+    elsif @vote.update( vote_params )
+      redirect_to vote_path(@vote.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    
+    @vote = Vote.find_by( id: params[:id] )
+    
+    if @vote.nil?
+      redirect_to votes_path
+      return
+    else
+      @vote.destroy
+      redirect_to votes_path
+      return
+    end
+  end
+  
+  
+  private             
+  
+  def vote_params
+    return params.require(:vote).permit(:name, :category, :title, :creator, :publication_year, :description)
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,10 +1,23 @@
 class Work < ApplicationRecord
+  has_many :votes
+  
   def self.top10(type)
     works = Work.where(category: type).all.order(title: :asc)
     return works[0...9]
   end
   
   def self.spotlight
+    #temp work item
     return Work.first
+  end
+  
+  def upvote
+    # creating a new vote with work id that's passed in as self
+    vote = Vote.new(id: self.id)
+    
+    # knows current user id
+    
+    # returns params we can use to make a new vote
+    
   end
 end
