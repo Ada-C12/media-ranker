b2e03a208a64d4c836b4938416e0684bbc2a5b0c
Brianna Kemp <kemp.bri@gmail.com>
2019-10-16 15:08:52 -0700

    Finished homepage

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,10 @@
 class Work < ApplicationRecord
+  def self.top10(type)
+    works = Work.where(category: type).all.order(title: :asc)
+    return works[0...9]
+  end
+  
+  def self.spotlight
+    return Work.find(27)
+  end
 end
