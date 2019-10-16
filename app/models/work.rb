3a540233b91e0f60bd5e5ae423896e23f8399f2d
Janice Huang <janicehuang@gmail.com>
2019-10-15 22:22:13 -0700

    add passing model tests for works

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,10 @@ class Work < ApplicationRecord
   validates :title, presence: true
   validates :category, presence: true
   validates :release_date, numericality: { only_integer: true}
+
+  def self.top_ten
+    works = Work.all
+    return works.sample(10)
+  end
+
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,11 +2,12 @@ require "test_helper"
 
 describe Work do
   describe 'validations' do
-    it 'is valid when title and category are present, and release date is an integer' do
+    before do
       @work = works(:leithauser)
+    end
+    it 'is valid when title and category are present, and release date is an integer' do
       expect(@work.valid?).must_equal true
     end
-
     it 'is invalid without a title' do
       @work.title = nil
       expect(@work.valid?).must_equal false
@@ -20,9 +21,23 @@ describe Work do
     end
 
     it 'is invalid if the release date is not an integer' do
-    @work.release_date = 'abc'
-    expect(@work.valid?).must_equal false
-    expect(@work.errors.messages).must_include :release_date
+      @work.release_date = 'abc'
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :release_date
+    end
+  end
+
+  describe 'relations' do
+    #TKTKTKTKTKTKTKTKTKTK
+  end
+
+  # describe 'custom methods' do
+  #   describe 'top_ten' do
+  #     it 'returns ten books' do
+        # all_works = [:hello, :gentleman, :camino, :sixty_nine, :heartbreaker, :amadeus, :whiteley, :immortalists, :believers, :leithauser, :abominable]
+
+      #   expect(.top_ten.length).must_equal 10
+      # end
     end
   end
 end
