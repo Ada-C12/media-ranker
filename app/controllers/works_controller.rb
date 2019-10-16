193d901782b5850d2b477b964775cf833d259406
Rinostar <codingrinostar@gmail.com>
2019-10-16 15:32:45 -0700

    Add show view for works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,8 @@
 class WorksController < ApplicationController
   def index
     @works = Work.all
-    @albums = Work.where(category: album)
-    @books = Work.where(category: book)
+    @albums = Work.where(category: 'album')
+    @books = Work.where(category: 'book')
   end
 
   def new
