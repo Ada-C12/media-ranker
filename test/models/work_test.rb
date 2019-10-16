83087acef50713eb7922f4332e0b283f0ea7105d
Hallie <idhallie@gmail.com>
2019-10-15 20:41:05 -0700

    Added Work model tests for validations.

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    before do
+      # Arrange
+      @work = Work.new(category: "movie", title: "A River Runs Through It", creator: "Robert Redford", publication_year: 1991, description: "Fly fishing in Montana")
+    end
+    
+    it 'is valid when all fields are present' do
+      expect(@work.valid?).must_equal true
+    end
+    
+    it 'is valid if the title is not unique but is a different category' do
+      @work.title = works(:blue).title
+      
+      expect(@work.valid?).must_equal true
+    end
+    
+    it 'is invalid without a title' do
+      @work.title = nil
+      
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title
+    end
+    
+    it 'is invalid if the title is not unique within the same category' do
+      @work.title = works(:rocky).title
+      
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title
+    end
+  end
 end
