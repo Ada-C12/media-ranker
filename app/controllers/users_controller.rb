fdef6cae5d1af30e85fa8dfada4912e9379f3497
Paige Davenport <davenport.paige@outlook.com>
2019-10-16 14:26:37 -0700

    Added section to application.html.erb to show flash messages if any.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,12 @@ class UsersController < ApplicationController
 
     if found_user
       session[:user_id] = found_user.id
-      flash[:message] = "Successfully logged in as existing user #{found_user.name}"
+      flash[:success] = "Successfully logged in as existing user #{found_user.name}"
     else
       new_user = User.new(name: username)
       if new_user.save
         session[:user_id] = new_user.id
-        flash[:messge] = "Successfully created new user #{new_user.name} with ID #{new_user.id}"
+        flash[:success] = "Successfully created new user #{new_user.name} with ID #{new_user.id}"
       else
         flash.now[:error] = "A problem occurred: Could not log in"
         return render :login
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   def logout
     #TODO add something if user was not logged in
     session[:user_id] = nil
-    flash[:message] = "Successfully logged out"
+    flash[:success] = "Successfully logged out"
     return redirect_to root_path
   end
 
