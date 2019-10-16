1fa30dc9390ffbc48708a81eb76766882f04c19b
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-16 16:59:27 -0700

    Added custom methods, tests, and fixtures for Works model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,18 @@
 class Work < ApplicationRecord
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true, numericality: { only_integer: true, greater_than: 1000 }
+
+  def self.spotlight_media
+    return Work.all.sample
+  end
+
+  def self.top_books
+
+  end
+
+  def self.top_albums
+  end
+
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
-describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+describe ApplicationHelper, :helper do
+  describe 'validation' do
+    before do
+      @obama = works(:obama)
+    end
+
+    it "is valid when all fields are present" do
+      value(@obama).must_be :valid?
+    end
+
+    it "is invalid without a title" do
+      @obama.title = nil
+
+      expect(@obama.valid?).must_equal false
+    end
+
+    it "is invalid without a creator" do
+      @obama.creator = nil
+
+      expect(@obama.valid?).must_equal false
+    end
+
+    it "is invalid without a category" do
+      @obama.category = nil
+
+      expect(@obama.valid?).must_equal false
+    end
+
+    it "is invalid without a publishing year" do
+      @obama.publication_year = nil
+
+      expect(@obama.valid?).must_equal false
+    end
+
+    it "is invalid with a publishing year greater than 1000" do
+      @obama.publication_year = 1000
+
+      expect(@obama.valid?).must_equal false
+    end
+  end
 end
