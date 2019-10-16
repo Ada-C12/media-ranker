b3681132daca966db3752dae59beba9cca4b969f
geli-gel <angele.zam@gmail.com>
2019-10-15 23:10:25 -0700

    added work model class methods to return works by category in hash

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,21 @@
 class Work < ApplicationRecord
+  def self.all_works_categorized
+    all_works_categorized = {}
+    all_works_categorized[:albums] = Work.where(category: "album")
+    all_works_categorized[:books] = Work.where(category: "book")
+    all_works_categorized[:movies] = Work.where(category: "movie")
+    return all_works_categorized
+  end
+  
+  def self.top_ten_categorized
+    top_ten_categorized = {}
+    top_ten_categorized[:albums] = Work.where(category: "album").sample(10)
+    top_ten_categorized[:books] = Work.where(category: "book").sample(10)
+    top_ten_categorized[:movies] = Work.where(category: "movie").sample(10)
+    return top_ten_categorized
+  end
+
+  def self.spotlight
+    return Work.all.sample
+  end
 end
