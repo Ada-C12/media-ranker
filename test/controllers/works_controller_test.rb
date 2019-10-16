bff60c312f321a2e39967a036babea7fcf13c097
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-16 14:45:39 -0700

    Added yml file & updated test helper

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe WorksController do
+describe Work do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
@@ -12,18 +12,18 @@ describe WorksController do
       must_respond_with :success
     end
   end
-
+  
   # show
   describe "show" do
     it "can get a work" do
-      work = Work.find(1)
-
+      work = Work.find_by(creator: "Wanda Gag")
+      
       get work_path(work.id)
-
+      
       must_redirect_to work_path(work.id)
     end
   end
-
+  
   # create
   describe "create" do
     it "can create a new work" do
@@ -35,7 +35,7 @@ describe WorksController do
         description: "Cool new work"
       }
       expect{ Work.create(params) }.must_differ "Work.count", 1
-
+      
       work = Work.find_by(title: "New work", category: "album")
       expect(work.creator).must_equal "New creator"
       expect(work.publication_year).must_equal 2000
@@ -43,11 +43,11 @@ describe WorksController do
       
     end
   end
-
+  
   # edit
   describe "edit" do
   end
-
+  
   # destroy
   describe "destroy" do
   end
