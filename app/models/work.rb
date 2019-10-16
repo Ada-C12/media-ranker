caa2f2ecef45630d90f74fd50c2f14159a1e7bc3
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-16 16:00:44 -0700

    More tests added

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,10 @@
 class Work < ApplicationRecord
   # validate presence & uniqueness
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :category }
+  
+  def category
+    where(:category => self.category)
+  end
+  
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -12,39 +12,61 @@ describe Work do
         p work.creator
         p work.publication_year
       end
-
+      
       # Arrange
-      work = Work.find_by(title: "Green Eggs and Ham")
+      work = Work.find_by(title: "Millions of Cats")
       # Act
+      is_valid = work.valid?
       
       # Assert
-      
+      assert( is_valid )
     end
     
     it "invalidates a work with no title" do
       # Arrange
+      work = Work.new(category: "book")
       # Act
+      is_valid = work.valid?
+      
       # Assert
+      refute( is_valid )
+      
     end
     
     it "invalidates a work whose title is not unique within the scope of the category" do
       # Arrange
+      work = Work.new(category: "book", title: "Millions of Cats")
       # Act
+      is_valid = work.valid?
+      
       # Assert
+      refute( is_valid )
     end
   end
   
   describe "custom methods" do
     it "can get the top ten works by vote" do
       # Arrange
+      top_ten = Work.top_ten("album")
+      
       # Act
+      
       # Assert
+      expect(top_ten.length).must_equal 10
+      expect(top_ten.first.id).must_equal Work.first.id
     end
     
     it "retrieves fewer than ten works if fewer than ten are in the category" do
       # Arrange
+      top_ten = Work.top_ten("movie")
+      
       # Act
+      
       # Assert
+      expect(top_ten.length).must_equal Work.all.length
+      expect(top_ten.length).must_be_less_than 10
+      expect(top_ten.first.id).must_equal Work.first.id
+      
     end
   end
   
