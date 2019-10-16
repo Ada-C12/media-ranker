de34a4e0bf31d8a6f36571187a481f72e2925ccc
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-15 23:07:10 -0700

    added works show, new, and _form files, changed data type of publication_date attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_015347) do
+ActiveRecord::Schema.define(version: 2019_10_16_034926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_015347) do
     t.string "category"
     t.string "title"
     t.string "creator"
-    t.date "publication_year"
+    t.string "publication_year"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
