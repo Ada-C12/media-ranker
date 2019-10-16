fc31621a45162259b0bb531a79d7470f0579499b
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-16 15:32:58 -0700

    Added tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -6,20 +6,45 @@ describe Work do
   # end
   describe "validations" do
     it "correctly validates a work with a unique title" do
+      Work.all.each do |work|
+        p work.id
+        p work.title
+        p work.creator
+        p work.publication_year
+      end
+
+      # Arrange
+      work = Work.find_by(title: "Green Eggs and Ham")
+      # Act
+      
+      # Assert
+      
     end
     
     it "invalidates a work with no title" do
+      # Arrange
+      # Act
+      # Assert
     end
     
     it "invalidates a work whose title is not unique within the scope of the category" do
+      # Arrange
+      # Act
+      # Assert
     end
   end
   
   describe "custom methods" do
     it "can get the top ten works by vote" do
+      # Arrange
+      # Act
+      # Assert
     end
     
     it "retrieves fewer than ten works if fewer than ten are in the category" do
+      # Arrange
+      # Act
+      # Assert
     end
   end
   
