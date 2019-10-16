7ba52ca55e0a25023dd33bfb61da7e8bcdac1fcd
Hallie <idhallie@gmail.com>
2019-10-15 21:35:23 -0700

    Created edit/update actions and view.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -12,4 +12,34 @@ class WorksController < ApplicationController
       return
     end
   end
+  
+  def edit
+    @work = Work.find_by(id: params[:id])
+    
+    if @work.nil?
+      redirect_to works_path
+      return
+    end 
+  end
+  
+  def update
+    @work = Work.find_by(id: params[:id])
+    
+    if @work.nil?
+      redirect_to works_path
+      return
+    elsif @work.update(work_params)
+      redirect_to work_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+  
+  private
+  
+  def work_params
+    params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
