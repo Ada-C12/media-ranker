0a1bed3b0af0b5c264c16ff3221dcf90fd95ad93
Vi <vanessareigosa@gmail.com>
2019-10-15 21:25:41 -0700

    fixed view all media view page

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,5 +5,16 @@ class Work < ApplicationRecord
   message: "There is already a work with that name in this category" }
 
   #Each work must have at least a title that is unique within its category - album, book, or movie.
+  def self.spotlight
+  end 
+
+  def self.categories
+    @categories_of_works = {
+    "albums" => Work.where(category: "album"),
+    "books" => Work.where(category: "book"),
+    "movies" => Work.where(category: "movie")
+    }
+    return @categories_of_works
+  end
 
 end
