da4adf5353e526720a616feeb90a594ed40acfb1
Hallie <idhallie@gmail.com>
2019-10-15 20:59:06 -0700

    Created works#show action and view.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,4 +3,13 @@ class WorksController < ApplicationController
     @works = Work.all
     @categories = ["album", "book", "movie"]
   end
+  
+  def show
+    @work = Work.find_by(id: params[:id])
+    
+    if @work.nil?
+      redirect_to works_path
+      return
+    end
+  end
 end
