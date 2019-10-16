e5358fb03fead4bf315678489e2fb208ab30db5c
Mariya Burrows <mariya.burrows@gmail.com>
2019-10-15 18:53:57 -0700

    added bootstrap navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+  
+  def login
+    username = params[:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{username}"
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
+    
+    redirect_to root_path
+  end 
+  
+  def login_form
+    @user = user.new
+  end 
+  
+  def current 
+    @current_user = User.find_by(id: session[:user_id])
+    Raise
+    unless @current_user
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+    end
+  end
+  
+  def logout
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,22 +1,5 @@
 class Work < ApplicationRecord
+
+  #top 10 votes by category
   
-  def albums
-    # all_media = Work.all 
-    
-    # albums = []
-    
-    # all_media.each do |work|
-    #   if work.category == "album"
-    #     albums << work
-    #   end 
-    # end
-    
-    # return albums
-  end 
-  
-  def books
-  end
-  
-  def movies 
-  end
 end
