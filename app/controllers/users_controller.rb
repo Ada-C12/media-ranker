62e432358ef22017bd70c2950d56905cf515ac8e
Paige Davenport <davenport.paige@outlook.com>
2019-10-16 12:34:35 -0700

    Added user login form and updated method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,12 @@ class UsersController < ApplicationController
     found_user = User.find_by(name: username)
 
     if found_user
-      session[:user_id] = found_user.user_id
+      session[:user_id] = found_user.id
       flash[:message] = "Successfully logged in as existing user #{found_user.name}"
     else
       new_user = User.new(name: username)
       if new_user.save
-        session[:user_id] = new_user.user_id
+        session[:user_id] = new_user.id
         flash[:messge] = "Successfully created new user #{new_user.name} with ID #{new_user.id}"
       else
         flash.now[:error] = "A problem occurred: Could not log in"
