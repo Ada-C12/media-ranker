3f9293725c7aa40fa6b75cbf5402b21f2be48553
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-10-15 18:57:55 -0700

    added show method/view and refactored index view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,15 +1,20 @@
 class WorksController < ApplicationController
   def index
-		@movies = Work.by_category("movie")
-		@books = Work.by_category("book")
-		@albums = Work.by_category("album")
+    @movies = Work.by_category("movie")
+    @books = Work.by_category("book")
+    @albums = Work.by_category("album")
   end
 
-	def show
-		
+  def show
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+		end
+
   end
 
-  def new
+	def new
   end
 
   def create
