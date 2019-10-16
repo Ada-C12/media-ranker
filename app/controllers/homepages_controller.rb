6b5d6b1d7474992639f59486d195b59b2b2ffbe9
Eve Le <nguyenthao.lee@gmail.com>
2019-10-16 11:40:03 -0700

    Update homepage to show spotlight and top works

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,8 @@
 class HomepagesController < ApplicationController
   def index
+    @spotlight_work = Work.highest_rated_work
+    @top_books = Work.top_works_sorted_by_category("book")
+    @top_movies = Work.top_works_sorted_by_category("movie")
+    @top_albums = Work.top_works_sorted_by_category("album")
   end
 end
