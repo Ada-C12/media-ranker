1f23e640596e83c121ee6160795a2154931ff0d7
Janice Huang <janicehuang@gmail.com>
2019-10-15 21:34:13 -0700

    tweak validations, add error message generator

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,5 @@ class Work < ApplicationRecord
   has_many :votes#, dependent: :nullify
   validates :title, presence: true
   validates :category, presence: true
-  #validates :title, presence: true
-
+  validates :release_date, numericality: { only_integer: true}
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    it 'is invalid without a title' do
+    end
+  end
+end
+
+
+  describe 'relations' do
+    it "has an author" do
+      book = books(:poodr)
+      book.author.must_equal authors(:metz)
+    end
+
+    it "can set the author" do
+      book = Book.new(title: "test book")
+      book.author = authors(:metz)
+      book.author_id.must_equal authors(:metz).id
+    end
+  end
 end
