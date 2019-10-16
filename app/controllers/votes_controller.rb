f96806e2ec90c640c5e2ffcb02b0c742b3fcf272
Janice Huang <janicehuang@gmail.com>
2019-10-16 16:09:04 -0700

    make wording tweaks

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,9 +2,9 @@ class VotesController < ApplicationController
   def create
     vote = Vote.where(:user => session[:user_id], :work => params[:work_id] )
     if vote # already voted
-      flash[:error] = "You have already voted for this work!"
+      flash[:error] = "Sorry, you may only vote once for this work!"
     else
-    @vote = Vote.create(date: Date.today)
+    vote = Vote.create(date: Date.today)
     end
   end
 end
