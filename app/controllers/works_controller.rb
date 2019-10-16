5fabff910ec12a159cd0d9bec426d580e2d60e5c
Eve Le <nguyenthao.lee@gmail.com>
2019-10-15 23:46:42 -0700

    Add view and implementation for works#index action

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,7 @@
 class WorksController < ApplicationController
+  def index
+    @books = Work.works_by_category("book")
+    @movies = Work.works_by_category("movie")
+    @albums = Work.works_by_category("album")
+  end
 end
