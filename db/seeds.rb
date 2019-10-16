b5cf9cc6918a6b56705c3780edf413344c502ad1
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-10-15 22:46:44 -0700

    sets up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,98 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# before creation, make a user at ID "0"
+
+
+deleted_user = User.new
+deleted_user.id = 0
+deleted_user.username = "previously_deleted" 
+deleted_user.save
+puts "Created deleted user"
+
+users = [
+{username: "Awesome Allias"},
+{username: "Brilliant Banda"},
+{username: "Crawfish Cartoon"},
+{username: "Doris Doris?"},
+{username: "Esurprise Surprise"},
+{username: "Fooey the Foolish"}, 
+{username: "Gorry Gorilla"}
+]
+
+users.each do |profile|
+  user = User.create(profile)
+end
+puts "Created standard users"
+
+works = [
+{name: "Bodhizafa",
+category: "movie",
+description: "awesome IPA",
+creator: "Georgetown",
+publication_year: "2011"},
+
+{name: "Spacedust IPA",
+category: "movie",
+description: "delicious IPA",
+creator: "Elysian",
+publication_year: "2016"}, 
+
+{name: "Dang!",
+category: "movie",
+description: "sharp IPA",
+creator: "Hellbent",
+publication_year: "2000"},
+
+{name: "Right One and a Half",
+category: "album",
+description: "From a cross body lead, from three",
+creator: "Micheal",
+publication_year: "2017"}, 
+
+{name: "Hammerlock",
+category: "album", 
+description: "From open position, left turn holding the right", 
+creator: "Momo", 
+publication_year: "2018"}, 
+
+
+{name: "The Martian",
+category: "book", 
+description: "Fear my botany powers", 
+creator: "Andy Weir", 
+publication_year: "2011"},
+
+
+{name: "Everything's Trash but It's OK",
+category: "book", 
+description: "The Bachelor", 
+creator: "Pheobe Robinson", 
+publication_year: "2018"}
+]
+
+
+works.each do |profile|
+  work = Work.create(profile)
+end
+puts "Created works"
+
+votes = [
+{user_id: 1, work_id: 1}, 
+{user_id: 1, work_id: 2}, 
+{user_id: 2, work_id: 5}, 
+{user_id: 3, work_id: 1}, 
+{user_id: 3, work_id: 2}, 
+{user_id: 4, work_id: 1}, 
+{user_id: 5, work_id: 4}, 
+{user_id: 6, work_id: 1}
+]
+
+votes.each do |profile|
+  vote = Vote.create(profile)
+end
+puts "Created votes"
