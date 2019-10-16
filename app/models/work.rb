b4205d784e0ef95884422c0152d13ea7fdde399e
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-10-16 11:49:09 -0700

    added css to application css

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,6 @@ class Work < ApplicationRecord
   validates :category, presence: true, inclusion: { in: ["album", "book", "movie"] }
   validates :title, presence: true, uniqueness: {scope: :category}
   # has_many :votes
+
+  
 end
