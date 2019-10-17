3212dad089fb893fd194a9e37492c1e4690b1f3b
Janice Huang <janicehuang@gmail.com>
2019-10-17 14:11:50 -0700

    get log in-log out-currently-sign-in-as functioning properly and in nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,21 +13,21 @@ class UsersController < ApplicationController
     if user # a user is found
       session[:user_id] = user.id
       flash[:success] = "Succesfully logged in as returning user #{username}"
-      redirect_to user_path(user.id)
+      redirect_to works_path
       return
     else
       user = User.create(username: username)
       session[:user_id] = user.id
       flash[:success] = "Succesfully logged in as new user #{username}"
     end
-    redirect_to user_path(user.id)
+    redirect_to works_path
     return
   end
 
-  def current 
+  def current
     @current_user = User.find_by(id: params[:id])
     unless @current_user
-      flash[:error] = "You must be logged in to see this page"
+      flash[:error] = "You must be logged in to vote"
       redirect_to root_path
     end
   end
