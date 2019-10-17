3fa082f0076d481dea96c8ba70535cdb69466d09
Hallie <idhallie@gmail.com>
2019-10-17 16:15:40 -0700

    Added model tests for top_voted and top_ten. Passes.

--- app/models/work.rb
+++ app/models/work.rb
@@ -9,17 +9,14 @@ class Work < ApplicationRecord
     return sorted_media
   end 
   
-  def self.top_voted(category)
-    Work.where(category: category)
+  def self.top_voted
+    return Work.order(votes_count: :desc).first
   end
   
   def self.top_ten(category)
     sorted_media = self.media_sort(category)
     arrange_by_votes = sorted_media.order(votes_count: :desc)
     
-    # sorted_media.includes(:votes).group(['user_id'])
     return arrange_by_votes[0..9]
-    # top_ten = Work.all.sample(10)
-    # return top_ten
   end
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -32,10 +32,18 @@ describe Work do
     end
   end
   
-  # describe 'relations' do
-  #   it "has many votes" do
-  #   end
-  # end
+  describe 'relations' do
+    it 'a work can have many votes' do
+      work = works(:rocky)
+      expect(work.votes.count).must_equal 2
+    end
+    
+    it 'a vote belongs to a user and a work' do
+      vote = votes(:hal_movie)
+      expect(vote.user_id).must_equal users(:hallie).id
+      expect(vote.work_id).must_equal works(:rocky).id
+    end
+  end
   
   describe 'custom methods' do
     it 'properly sorts works by specified category' do
@@ -45,6 +53,25 @@ describe Work do
       expect( book_list[0].category ).must_equal 'book'
       expect( book_list[-1].category ).must_equal 'book'
       expect( book_list.count ).must_equal 10
+    end
+    
+    it 'properly identifies the top voted work when there are votes' do
+      top_voted = Work.top_voted
+      
+      expect(top_voted).must_equal works(:winter)
+    end
+    
+    it 'identifies the top ten voted works by category' do
+      top_albums = Work.top_ten('album') #There are 11 voted albums
+      
+      expect(top_albums.count).must_equal 10
+      expect(top_albums.first).must_equal works(:winter)
+      expect(top_albums.last).must_equal works(:goodbye)
+      
+      top_movies = Work.top_ten('movie') #There are 0 voted movies
+      
+      expect(top_movies.count).must_equal 3
+      
     end
   end
 end
