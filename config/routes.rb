63eced598a6dfc02ea596eb220f094e8aca10bf0
Brianna Kemp <kemp.bri@gmail.com>
2019-10-17 10:23:29 -0700

    Added users routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :works do
     resources :votes, only: [:new, :create]
   end
-  
   resources :users
+  
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
