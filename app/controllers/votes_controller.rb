9bf2ddf835df40db6dc1022fc8d53b210ad36200
Hallie <idhallie@gmail.com>
2019-10-17 10:41:23 -0700

    upvote method actually works now.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,18 +3,24 @@ class VotesController < ApplicationController
     work = Work.find_by(id: params[:work_id])
     user = User.find_by(id: session[:user_id])
     
-    if user.nil?
-      flash.now[:failure] = "A problem occurred: You must log in to do that."
-        redirect_to work_path
-      elsif work.votes.find_by(user_id: user.id)
-        flash.now[:failure] = "Could not upvote. #{user.username} has already voted for this work."
-        render :new
-        return
-      else
-        Vote.new(user_id: user.id, work_id: work.id)
-        flash[:success] = "Successfully upvoted!"
-        redirect_to works_path
-      end 
+    
+    if user.nil? # If there is no user signed in
+      flash[:failure] = "A problem occurred: You must log in to do that."
+      redirect_to works_path
+      return
+    elsif work.votes.find_by(user_id: user.id) # If this user has already voted for the work
+      flash[:failure] = "Could not upvote. #{user.username} has already voted for this work."
+      redirect_to works_path
+      return
     end
+    
+    vote = Vote.new(user_id: user.id, work_id: work.id)
+    if vote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_to works_path
+      return
+    else
+      flash.now[:failure] = "Upvote failed."
+    end 
   end
-  
+end
