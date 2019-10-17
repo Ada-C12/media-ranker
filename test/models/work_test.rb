a7bbd2f25632bfb95ec4a7353ec9b178803f0e1f
dora1405 <li.dora05@gmail.com>
2019-10-17 15:20:34 -0700

    Works_test for validation complete

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "validations" do
+
+    it "can be valid" do
+      is_valid = works(:work1).valid?
+
+      assert( is_valid )
+
+    end
+
+    it "will respond will error if invalid data" do
+      is_invalid = works(:work1)
+      is_invalid.title = nil
+
+      # Assert
+      expect(is_invalid.valid?).must_equal false
+      expect(is_invalid.errors.messages).must_include :title
+      expect(is_invalid.errors.messages[:title]).must_equal ["can't be blank"]
+
+    end
+  end
 end
