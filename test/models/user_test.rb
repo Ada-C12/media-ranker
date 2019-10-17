da12201206f380e193df730958b2d2d9383a908f
dora1405 <li.dora05@gmail.com>
2019-10-17 15:23:13 -0700

    Users_test for validations complete

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+
+    it "can be valid" do
+      is_valid = users(:user1).valid?
+
+      assert( is_valid )
+
+    end
+
+    it "will respond will error if invalid data" do
+      is_invalid = users(:user1)
+      is_invalid.username = nil
+
+      # Assert
+      expect(is_invalid.valid?).must_equal false
+      expect(is_invalid.errors.messages).must_include :username
+      expect(is_invalid.errors.messages[:username]).must_equal ["can't be blank"]
+
+    end
+  end
 end
