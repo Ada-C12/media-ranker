e9f1c3b621186cc89d650f924b1d44487acfebcb
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-17 13:50:45 -0700

    changed schema publication year data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_225551) do
+ActiveRecord::Schema.define(version: 2019_10_17_203349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2019_10_15_225551) do
     t.string "category"
     t.string "title"
     t.string "creator"
-    t.string "publication_year"
     t.string "description"
+    t.integer "publication_year"
   end
 
 end
