2d9c0cd9998d937f8b665306f89d1c0327698b4e
Eve Le <nguyenthao.lee@gmail.com>
2019-10-16 18:15:22 -0700

    Add users#login_form users#login and users#logout action and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,38 @@ class UsersController < ApplicationController
       head :not_found
     end
   end
+
+  def login_form
+    @user = User.new
+  end
+  
+  def login
+    username = params[:user][:username]
+    if username.nil? || username.strip == ""
+      flash[:error] = "Username cannot be blank!"
+      redirect_to login_path
+      return
+    end
+
+    @user = User.find_by(username: params[:user][:username])
+    if @user
+      flash[:success] = "Logged in!"
+      session[:user_id] = @user.id      
+    else
+      @user = User.create(username: params[:user][:username])
+      flash[:success] = "User successfully created!"
+      session[:user_id] = @user.id
+    end
+    redirect_to user_path(@user.id)
+    return
+  end
+
+  def logout
+    if session[:user_id]
+        session[:user_id] = nil
+        flash[:success] = "You have logged out"
+        redirect_to works_path
+        return
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :works
   resources :users
   resources :votes
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
 end
