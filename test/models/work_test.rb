b24cd0354dca3b0dd1403220097c41680a9db4c7
geli-gel <angele.zam@gmail.com>
2019-10-17 13:49:41 -0700

    work tests in progress

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -47,23 +47,60 @@ describe Work do
       expect(new_work.errors.messages).must_include :title
       expect(new_work.errors.messages[:title]).must_equal ["can't be blank"]
     end
+    it "must have a unique title within its category" do
+      # Arrange
+      existing_album_title = works(:album).title
+      # Act
+      new_album_same_title = Work.create(category: "album", title: existing_album_title, creator: "gamooli", publication_year: 2016, description: "are you trying to add an album with the same title as one we already have?")
+      # Assert
+      expect(new_album_same_title.valid?).must_equal false
+      expect(new_album_same_title.errors.messages).must_include :title
+      expect(new_album_same_title.errors.messages[:title]).must_equal ["has already been taken"]
+    end
+
+    it "allows non-unique titles across categories" do
+      # Arrange
+      existing_album_title = works(:album).title
+      # Act
+      new_movie_same_title = Work.create(category: "movie", title: existing_album_title, creator: "IDK", publication_year: 2016, description: "who knows")
+      # Assert
+      expect(new_movie_same_title.valid?).must_equal true
+    end
+
   end
 
   # Tests for methods you create should go here
   describe "custom methods" do
+    before do
+      @categories = ["album", "book", "movie"]
+    end
     describe "all_works_categorized" do
       # Your code here
-      it "splits the work objects into categories and stores them in a hash" do
+      it "splits the work objects into categories (plural) and stores them in a hash" do
         # Arrange
           # make sure there is one work of each category in the test database
+          Work.delete_all
+          @categories.each do |category|
+            Work.create(category: category, title: "Any kind of #{category}!", creator: "Who knows", publication_year: 2016, description: "This #{category} is VERY SCAREY" )
+          end
+          expect(Work.count).must_equal @categories.length
         # Act
           # store Work.all_works_categorized into a variable
+          all_works_categorized = Work.all_works_categorized
         # Assert
           # expect the variable:
             # is a hash
+          expect(all_works_categorized).must_be_instance_of Hash
             # is of the same length as there are categories
+          expect(all_works_categorized.length).must_equal @categories.length
             # has values where the values.category == key
+          all_works_categorized.each do |category, works|
+            works.each do |work|
+              expect(work.category).must_equal category.to_s.singularize
+            end
+          end
       end
+
       it "stores the hash value as an empty array if there are no works for the given category/key" do
         # Arrange
           # make sure there are no works in the test database
