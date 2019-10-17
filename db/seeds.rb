c00a6e662db4d714312f99b1a4f0fe7d66da4d99
Brianna Kemp <kemp.bri@gmail.com>
2019-10-17 10:12:42 -0700

    Updated seeds file and added starter data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
-FILE = Rails.root.join('db', 'media-seeds.csv')
-puts "Loading raw data from #{FILE}"
+MEDIA_FILE = Rails.root.join('db', 'media-seeds.csv')
+puts "Loading raw data from #{MEDIA_FILE}"
 
-failures = []
-CSV.foreach(FILE, :headers => true) do |row|
+media_failures = []
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
   work = Work.new
   work.category = row['category']
   work.title = row['title']
@@ -20,7 +20,7 @@ CSV.foreach(FILE, :headers => true) do |row|
   work.description = row['description']
   successful = work.save
   if !successful
-    failures << work
+    media_failures << work
     puts "Failed to save work: #{work.inspect}"
   else
     puts "Created work: #{work.inspect}"
@@ -28,8 +28,27 @@ CSV.foreach(FILE, :headers => true) do |row|
 end
 
 puts "Added #{Work.count} records"
-puts "#{failures.length} records failed to save"
+puts "#{media_failures.length} records failed to save"
 
+
+USER_FILE = Rails.root.join('db', 'users-seeds.csv')
+puts "Loading raw data from #{USER_FILE}"
+
+user_failures = []
+CSV.foreach(USER_FILE, :headers => true) do |row|
+  user = User.new
+  user.username = row['username']
+  successful = user.save
+  if !successful
+    user_failures << user
+    puts "Failed to save work: #{user.inspect}"
+  else
+    puts "Created work: #{user.inspect}"
+  end
+end
+
+puts "Added #{User.count} records"
+puts "#{user_failures.length} records failed to save"
 
 # Since we set the primary key (the ID) manually on each of the
 # tables, we've got to tell postgres to reload the latest ID
