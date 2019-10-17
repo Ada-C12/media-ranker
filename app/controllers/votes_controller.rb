cbae1ae5519675b98b042e9334553a98962a2bed
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-16 22:49:59 -0700

    duplicate votes by same user will get flash/alert and stay on same page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,7 @@ class VotesController < ApplicationController
   
   def create
     
+    # first check if user is logged in
     if !session[:user_id]
       flash[:error] = "Must be logged in to vote"
       
@@ -17,13 +18,27 @@ class VotesController < ApplicationController
         return
       end
       
-    else    
-      # vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
-      
+    else   
+      # then check if user already voted for this work
       user = User.find_by(id: session[:user_id])
-      work = Work.find_by(id: params[:work_id])
-      vote = Vote.new(user: user, work: work)
+      if user
+        if user.votes.any?
+          first_vote = user.votes.select { |vote| vote.work_id == params[:work_id].to_i }
+          if first_vote.any?
+            flash[:error] = "You already voted for this, no double dipping!"
+            first_vote = nil
+            redirect_to_origin
+            # weird, redirect_to_origin was written with return built in, and worked fine on line 13 above, but not here... JARED??!!
+            return
+          end
+        end
+      else
+        flash[:error] = "Impossible, session is set when user logged in! Investigate!"
+        redirect_to nope_path
+        return
+      end
       
+      vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
       if vote.save
         flash[:success] = "Successfully upvoted!"
         redirect_to_origin
@@ -40,8 +55,10 @@ class VotesController < ApplicationController
   private 
   def redirect_to_origin
     # session[:origin_prefix] should be set as the prefix of the get page we want to return to by this method
-    # this method will 1. reset session[:origin_prefix] = nil
-    # 2. redirect_to session[:origin_prefix]_path
+    # this method will ...
+    # 1. reset session[:origin_prefix] = nil
+    # 2. redirect_to session[:origin_prefix]_path, followed by return 
+    # 3. WTF, step2's return doesn't always seem to work, sometimes need to add return after calling this method, idk why
     
     if session[:origin_prefix] == "work"
       redirect_to work_path(id: params[:work_id])
