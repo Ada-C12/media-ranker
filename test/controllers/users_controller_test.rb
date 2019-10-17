099b4227d1d6d3f4cc0dee04f34ea85bd8557845
Brianna Kemp <kemp.bri@gmail.com>
2019-10-17 10:22:49 -0700

    Added users controller test, one not passing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "responds with not_found when user hasn't logged in" do
+    get current_user_path
+    
+    must_respond_with :not_found
+  end
+  
+  it "responds with success when user has logged in" do
+    user = User.first
+    params = {
+      user: {
+        username: user.username
+      }
+    }
+    puts "user info:"
+    
+    p user 
+    post login_path(params)
+    
+    #TO-DO intial code user_id my model is just id
+    expect(session[:user_id]).must_equal user.id
+    
+    puts "test:"
+    p expect(session[:user_id]).must_equal user.id
+    get current_user_path
+    p user
+    p current_user_path
+    must_respond_with :success
+  end
 end
