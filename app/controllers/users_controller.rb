0dbd83a719d53d59e5906f7dddf13e6edbfca4f8
Eve Le <nguyenthao.lee@gmail.com>
2019-10-16 18:14:05 -0700

    Add users#show action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      head :not_found
+    end
+  end
 end
