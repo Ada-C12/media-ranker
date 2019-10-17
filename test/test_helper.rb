a5270995a17a6a90941763b13a39e93da4ab797e
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-17 14:50:22 -0700

    code updated

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,22 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def perform_login(user = nil)
+    user = User.first
+    
+    params = {
+      user: {
+        username: user.username
+      }
+    }
+
+    post login_path(params)
+
+    # Verify the user ID was saved - if that didn't work, this test is invalid
+    expect(session[:user_id]).must_equal user.id
+
+    return user
+
+
+  end
 end
