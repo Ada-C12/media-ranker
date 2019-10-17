255fa21bda6c4e6625558808f104748ca59af50e
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-16 17:18:51 -0700

    references added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_184430) do
+ActiveRecord::Schema.define(version: 2019_10_16_234754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2019_10_16_184430) do
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "work_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -34,4 +38,6 @@ ActiveRecord::Schema.define(version: 2019_10_16_184430) do
     t.string "description"
   end
 
+  add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
