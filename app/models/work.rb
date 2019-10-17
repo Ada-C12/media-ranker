9b3cfc225ef77569f51b3401fce57e674f26f357
Michaela Morrow <morrow.michaela@gmail.com>
2019-10-16 17:27:22 -0700

    Created method 'top_ten' in work model and added it to homepage view

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,18 @@
 class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :category}
+  
+  def self.top_ten(category)
+    all_works = self.all
+    ten_works = []
+    all_works.each do |work|
+      if work.category == category
+        ten_works << work
+      end
+    end
+    return ten_works
+  end
+  
+  def self.spotlight
+    return self.all.first
+  end
 end
