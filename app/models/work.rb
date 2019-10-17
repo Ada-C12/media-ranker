e5b3e71742736af2bd2248db2fa37ce168e7e39e
Paige Davenport <davenport.paige@outlook.com>
2019-10-16 20:57:23 -0700

    Refactored top ten method.

--- app/models/work.rb
+++ app/models/work.rb
@@ -16,7 +16,7 @@ class Work < ApplicationRecord
 
   def self.top_ten(category)
     # if category needs to be validated, should that go in the controller? 
-    all_in_category = Work.where(category: category).order(votes_count: :asc)
+    all_in_category = Work.where(category: category).order('votes_count DESC NULLS LAST')
     top_ten = all_in_category.take(10)
     return top_ten
   end
