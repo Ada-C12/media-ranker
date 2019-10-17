78e60ef85ecbd838776d242ded2dc77fff919797
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-17 14:40:54 -0700

    Created tests for user model

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,32 @@ describe UsersController do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+  
+  # describe "login" do
+  #   it "can successfully login a new user" do
+  
+  #     user = User.create(username: "Josephine")
+  #     login_data = {
+  #       user: {
+  #         username: user.username
+  #       }
+  #     }
+  #     post login_path, params: login_data
+  
+  #     expect(session[:user_id]).must_equal user.id
+  
+  #     get user_path
+  
+  #     must_respond_with :success
+  #   end
+  #   it "can successfully login a returning user" do
+  #   end
+  #   it "does not login a user without a username" do
+  #   end
+  # end
+  
+  # describe "logout" do
+  #   it "can successfully logout a user" do
+  #   end
+  # end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,29 @@ describe User do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+  describe "validation" do
+    it "can create a valid user" do
+      user = User.create(username: "Dorothy")
+      
+      is_valid = user.valid?
+      
+      assert( is_valid )    
+    end
+    
+    it "will not login a user without a username" do
+      user = User.create(username: nil)
+      
+      is_valid = user.valid?
+      
+      refute( is_valid )    
+    end
+    
+    it "will not login a user with only spaces as a username" do
+      user = User.create(username: "  ")
+      
+      is_valid = user.valid?
+      
+      refute( is_valid )    
+    end
+  end
 end
