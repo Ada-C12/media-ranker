587a8bcd58927d1aa787737a445e985253769f9b
Rinostar <codingrinostar@gmail.com>
2019-10-16 20:39:47 -0700

    Test Work model validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,8 @@
 class Work < ApplicationRecord
+  serialize :votes, Hash
+
+  validates :category, presence: true
+  validates :title, presence: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true, numericality: { only_integer: true }
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:text_work) {
+    Work.new(
+      category: "book",
+      title: "text book",
+      creator: "anonymous",
+      publication_year: 2010
+    )
+  }
+
+  it "can be instantiated" do
+    expect(text_work.valid?).must_equal true
+  end
+
+  describe "validations" do 
+    it "must have a category" do
+      text_work.category = nil
+
+      expect(text_work.valid?).must_equal false
+      expect(text_work.errors.messages).must_include :category
+      expect(text_work.errors.messages[:category]).must_equal ["can't be blank"]
+    end
+
+    it "must have a title" do
+      text_work.title = nil
+
+      expect(text_work.valid?).must_equal false
+      expect(text_work.errors.messages).must_include :title
+      expect(text_work.errors.messages[:title]).must_equal ["can't be blank"]
+    end
+
+    it "must have a creator" do
+      text_work.creator = nil
+
+      expect(text_work.valid?).must_equal false
+      expect(text_work.errors.messages).must_include :creator 
+      expect(text_work.errors.messages[:creator]).must_equal ["can't be blank"]
+    end
+
+    it "must have a publication year" do
+      text_work.publication_year = nil
+
+      expect(text_work.valid?).must_equal false
+      expect(text_work.errors.messages).must_include :publication_year 
+      expect(text_work.errors.messages[:publication_year]).must_equal ["can't be blank", "is not a number"]
+    end
+  end
 end
