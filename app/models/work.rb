162dfb7fae74ac4ef1201d17b02dab881c2171b6
Diana <dianah46@gmail.com>
2019-10-17 16:29:57 -0700

    added validations to work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,12 +1,20 @@
 class Work < ApplicationRecord
   has_many :votes
 
-  validates :title, presence: true
+  validates :title, uniqueness: { scope: :category, message: "You can't add a work with the same title!"}
+
 
   # this method will be used in homepages view and works#index view
-  def album_list
-    albums = Work.where(category = "album")
-    albums.vote.order()
+
+
+  # 
+  def self.album_list
+
+    return Work
+      .where(category: "album")
+      .left_joins(:votes)
+      .group(:id)
+      .order('COUNT(votes.id) DESC')
 
     #iterate through each work to count the vote
   end
