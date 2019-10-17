9811ca937dcc200463260e32ac6426e1bf643f6e
Paige Davenport <davenport.paige@outlook.com>
2019-10-17 16:48:43 -0700

    Added user details page with list of votes.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ApplicationRecord
-  belongs_to :user, counter_cache: true
+  belongs_to :user
   belongs_to :work, counter_cache: true
 
   validates :user, uniqueness: { scope: :work,  message: "has already voted for this work" }
