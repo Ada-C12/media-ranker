536ae2fd276fe3d29fa32c3b1e50487c3dc87838
Janice Huang <janicehuang@gmail.com>
2019-10-17 12:38:25 -0700

    write validation tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    before do
+      @user = users(:january)
+    end
+    it 'is valid when username is present' do
+      expect(@user.valid?).must_equal true
+    end
+    it 'is invalid without a username' do
+      @user.username = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :username
+    end
+  end
+
+  describe 'relations' do
+  end
+
+  describe 'custom methods' do
+  end
+
 end
