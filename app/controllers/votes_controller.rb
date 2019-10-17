84185e068f6eb534cb9ad5e49bc0bc6b4984a1d2
Julia Bouvier <juliabouv@gmail.com>
2019-10-16 19:27:04 -0700

    Implemented validation for creating Vote object. User cannot vote twice on same work

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,27 @@
 class VotesController < ApplicationController
   def create
     work = Work.find_by(id: params[:work_id])
-    user = User.find_by(id: session[:user_id])
-    if work.nil? || user.nil?
+    if work.nil?
       head :not_found 
       return
     end
-    
+
+    user = User.find_by(id: session[:user_id])
+    if user.nil?
+      flash[:error] = "A problem occurred: You must log in to do that"
+      redirect_back(fallback_location: :back)
+      return
+    end
+
     @vote = Vote.new(work_id: work.id, user_id: user.id)
+
     if @vote.save
       flash[:success] = "Successfully upvoted!"
-      redirect_to work_path(work.id)
+      redirect_back(fallback_location: :back)
       return
     else
       flash[:error] = "A problem occurred: Could not upvote"
-      redirect_to work_path(work.id)
+      redirect_back(fallback_location: :back)
       return
     end
   end
