ea63ce8089903c933894acc482bc84b3360ca1e7
daniela sanchez <dnsanche@gmail.com>
2019-10-17 12:54:58 -0700

    Added tests for model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,4 @@
 class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-  validates :publication_year, numericality: true
   validates :category, presence: true
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
-describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe Work do
+    let (:new_work) {
+      Work.new(category: "book", title: "man of the goods", creator: "Josh Howe", publication_year: 2019, description: "gsgdfg")
+    }
+    it "can be instantiated" do
+      # Assert
+      expect(new_work.valid?).must_equal true
+    end
+
+    it "will have the required fields" do
+      # Arrange
+      [:category, :title, :creator, :publication_year, :description].each do |field|
+
+      # Assert
+      expect(works(:wind)).must_respond_to field
+    end
+  end
+
+  describe "validations" do
+    it "must have a title" do
+      # Arrange
+      works(:wind).title = nil      
+      # Assert
+      expect(works(:wind).valid?).must_equal false
+      expect(works(:wind).errors.messages).must_include :title
+      expect(works(:wind).errors.messages[:title]).must_equal ["can't be blank"]
+    end
+
+    it 'is invalid if the title is not unique' do
+      @work = Work.new(category: 'book', title: 'a', creator: 'hgkjhj', publication_year: 2019, description: 'jkjl')
+      @work.title = works(:dahlio).title
+
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title
+    end
+
+    it "must have a category" do
+      # Arrange
+      works(:wind).category = nil      
+      # Assert
+      expect(works(:wind).valid?).must_equal false
+      expect(works(:wind).errors.messages).must_include :category
+      expect(works(:wind).errors.messages[:category]).must_equal ["can't be blank"]
+    end
+  end
 end
