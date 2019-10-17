0197a5468829a1fd5c3031b4345aa0c5fff8fc33
Alice <alicesunhi@gmail.com>
2019-10-17 12:04:17 -0700

    added validation for user login to display errors on login_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,20 @@ class UsersController < ApplicationController
   def login
     username = params[:user][:username]
     user = User.find_by(username: username)
+    
     if user
       session[:user_id] = user.id
       flash[:success] = "Successfully logged in as returning user #{username}"
     else
-      user = User.create(username: username)
-      session[:user_id] = user.id
-      flash[:success] = "Successfully logged in as new user #{username}"
+      @user = User.create(username: username)
+      
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Successfully logged in as new user #{username}"
+      else
+        render :login_form
+        return
+      end
     end
     
     redirect_to root_path
@@ -21,7 +28,7 @@ class UsersController < ApplicationController
   def current
     @current_user = User.find_by(id: session[:user_id])
     unless @current_user
-      flash[:error] = "You must be logged in to view this page/"
+      flash[:error] = "You must be logged in to view this page."
       redirect_to root_path
     end
   end
