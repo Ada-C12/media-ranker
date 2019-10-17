f785478b696f903b6e5662dd3bb7fc896d98c78e
daniela sanchez <dnsanche@gmail.com>
2019-10-17 13:59:45 -0700

    Added methods for spotlight and top 10 homepage, updated homepage view

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,27 @@
 class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :category, presence: true
+
+  def self.spotlight
+    spotlight = Work.first
+    return spotlight
+  end
+  
+  def self.top_10_books
+    books = Work.where(category: "book")
+    top_10_books = books.sample(10)
+    return top_10_books
+  end
+
+  def self.top_10_albums
+    albums = Work.where(category: "album")
+    top_10_albums = albums.sample(10)
+    return top_10_albums
+  end
+
+  def self.top_10_movies
+    movies = Work.where(category: "movie")
+    top_10_movies = movies.sample(10)
+    return top_10_movies
+  end
 end
