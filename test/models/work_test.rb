1c9c38b4b4c95ca22c39bbe72675a5f8853c01e7
Julia Bouvier <juliabouv@gmail.com>
2019-10-17 14:38:09 -0700

    Implemented fixtures

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "can be instantiated" do
+    work = works(:movie)
+    expect(work.valid?).must_equal true
+  end
 end
