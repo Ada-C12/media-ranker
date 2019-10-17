0b1bd99af221bfeffdb7340543dae8b9df2383cb
Michaela Morrow <morrow.michaela@gmail.com>
2019-10-17 13:50:59 -0700

    Changed publication_year data type to integer; reseeded db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_225824) do
+ActiveRecord::Schema.define(version: 2019_10_17_203350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(version: 2019_10_15_225824) do
     t.string "category"
     t.string "title"
     t.string "creator"
-    t.string "publication_year"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "publication_year"
   end
 
 end
