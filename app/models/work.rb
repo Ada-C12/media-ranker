37297cdadcf8464b9be4b370d2f459ca20393bec
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-16 17:27:00 -0700

    created top ten method

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,18 @@
 class Work < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: { scope: :category, message: "has already been taken"}
+  
+  def self.top_ten(category)
+    all_works = self.all
+    top_ten_works = []
+    all_works.each do |work|
+      if work.category == category
+        top_ten_works << work
+      end
+    end
+    return top_ten_works
+  end
+
+  def spotlight
+  end
 end
