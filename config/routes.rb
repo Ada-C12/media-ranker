0830254ccffab8b00966d979c610118c3d0b80de
geli-gel <angele.zam@gmail.com>
2019-10-17 18:19:27 -0700

    user and works #show have votes tables, removed unnecessary nested route

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   root to: 'works#top_ten'
   
   get '/works/top_ten', to: 'works#top_ten', as: 'top_ten_works'
-  post '/work/:id/upvote', to: 'works#upvote', as: 'upvote_work'
+  post '/works/:id/upvote', to: 'works#upvote', as: 'upvote_work'
 
-  resources :works do
-    resources :votes, only: [:index, :post]
-  end
-  
+  resources :works 
+
   resources :users, except: :delete
+  
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
