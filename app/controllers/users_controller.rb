946e46e7bc622b26f1730f732722b126c0823c80
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-10-18 12:06:22 -0700

    added login/logout methods/view and updated nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,41 @@ class UsersController < ApplicationController
       return
     end
   end
+
+  def login_form 
+    @user = User.new 
+  end 
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{username}"
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
   
+    redirect_to root_path
+  
+  end
+ 
+  def logout
+    @user = User.find_by(id: session[:user_id])
+
+    if @user
+      session[:user_id] = nil
+      flash[:success] = "Successfully logged out"
+    else
+      flash[:error] = "Unsuccessfully logged out"
+    end
+    redirect_to root_path
+  end 
+
+  private
+  def user_params
+    return params.require(:user).permit(:username)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root to: "homepages#index"
-  resources :users 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+
 resources :works
 
 get "/login", to: "users#login_form", as: "login"
 post "/login", to: "users#login"
 post "/logout", to: "users#logout", as: "logout"
-get "/users/current", to: "users#current", as: "current_user"
+
 
 end
