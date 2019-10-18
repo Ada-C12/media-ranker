8c21fa0b0b530b3902314b6ef457552bc8aef5f5
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-18 14:41:35 -0700

    Basic styling for Homepages index view

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -7,15 +7,5 @@ describe HomepagesController do
 
       must_respond_with :success
     end
-
-    # it "responds with success when there are no works saved" do
-    #   works.each do |work|
-    #     work.destroy
-    #   end
-
-    #   get root_path
-
-    #   must_respond_with :success
-    # end
   end
 end
