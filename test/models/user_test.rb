b413c24d477ad7106e78603a39b346ccc43cf50c
Rinostar <codingrinostar@gmail.com>
2019-10-17 21:44:07 -0700

    User model validation & tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:text_user) {
+    User.new(name: 'SpongeBob')
+  }
+  
+  describe "initilaiztaion" do
+    it "can be instantiated" do
+      expect(text_user.valid?).must_equal true
+    end
+  end 
+
+  describe "validations" do 
+    it "must have a name" do
+      text_user.name = nil
+
+      expect(text_user.valid?).must_equal false
+      expect(text_user.errors.messages).must_include :name
+      expect(text_user.errors.messages[:name]).must_equal ["can't be blank"]
+    end
+  end
 end
