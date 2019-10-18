b5b6496bb1b5bebcafaea433688318a48d5e71f8
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-17 20:38:06 -0700

    half done with work model test, let_yml_superhash experiment seems ok, someone else might hate it though lol

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,45 +2,48 @@ require "test_helper"
 
 describe Work do
   
-  let (:let_hash) { let_turbo }
-  
-  
-  # let (:album1) { works(:album1) }
-  # let (:book1) { works(:book1) }
-  # let (:movie1) { works(:movie1) }
-  # let (:album2) { works(:album2) }
-  # let (:book2) { works(:book2) }
-  # let (:movie2) { works(:movie2) }
-  let (:some_yml_works) { [album1, book1, movie1, album2, book2, movie2] }
+  let (:yml) { let_yml_superhash }
   
   describe "RELATIONS" do
+    let(:movie1) { yml[:movie1] }
     it "can have many votes" do
-      db_album1 = get_db_work(yml_key: let_hash[:movie1])
-      p db_album1.votes.count
+      assert(movie1.votes.count == 4)
+      new_vote = Vote.create(work: movie1, user_id: yml[:user6].id)
+      assert(movie1.votes.count == 5)
     end
     
     it "workObj.votes exists and can return Vote objs" do
-      ### NEED Votes Model
-    end
-    
-    it "if vote gets casted, workObj.votes will reflect that" do
+      movie1.votes.each do |vote|
+        assert(vote.class == Vote)
+        assert(vote.work.title == movie1.title)
+      end 
     end
   end
   
   describe "VALIDATIONS" do
     it "Can create Work obj with correct attributes" do
-      some_yml_works.each_with_index do |piece, index|
-        assert(piece.valid?)
-        db_piece = Work.find_by(title: piece[:title])
-        attributes = [:title, :published_year, :creator, :category, :votes, :description]
-        attributes.each do |attrib|
-          if attrib == :votes
-            assert(db_piece[:votes] == nil)
-          else
-            assert(db_piece[attrib] == some_yml_works[index][attrib])
-          end
-        end
+      categories = %w[movie album book]
+      titles = %w[aaa bbb ccc]
+      pub_years = [123, 456, 789]
+      creators = %w[me you they]
+      descriptions = %w[xxx yyy zzz]
+      categories.each_with_index do |category, index|
+        Work.create!(category: categories[index], 
+          title: titles[index], 
+          published_year: pub_years[index], 
+          creator: creators[index], 
+          description: descriptions[index]
+        )
+        new_work = Work.last
+        
+        assert(new_work.category == categories[index])
+        assert(new_work.title == titles[index])
+        assert(new_work.published_year == pub_years[index])
+        assert(new_work.votes == [])
+        assert(new_work.creator == creators[index])
+        assert(new_work.description == descriptions[index])
       end
+      
     end
     
     describe "Won't create Work obj, given bogus inputs" do
@@ -62,7 +65,7 @@ describe Work do
       end
       
       it "bogus title" do
-        album1
+        album1 = yml[:album1]
         bogus3 = Work.new(category:"album", title:album1.title, published_year:album1.published_year)
         refute(bogus3.save)
         expect(bogus3.errors.messages.keys).must_include :title
@@ -77,78 +80,78 @@ describe Work do
         end
       end
       
-      it "bogus published_year" do
-        bad_args = ["", nil, "  "]
-        bad_args.each do |bad_arg|
-          bogus5 = Work.new(category:"album", title: "ok", published_year: bad_arg)
-          refute(bogus5.save)
-          expect(bogus5.errors.messages.keys).must_include :published_year
-          expect(bogus5.errors.messages.values).must_include ["can't be blank"]
-        end
-      end
+      # it "bogus published_year" do
+      #   bad_args = ["", nil, "  "]
+      #   bad_args.each do |bad_arg|
+      #     bogus5 = Work.new(category:"album", title: "ok", published_year: bad_arg)
+      #     refute(bogus5.save)
+      #     expect(bogus5.errors.messages.keys).must_include :published_year
+      #     expect(bogus5.errors.messages.values).must_include ["can't be blank"]
+      #   end
+      # end
     end
   end
   
-  describe "METHOD: spotlight_winner()" do
-    it "nominal case" do
-      # need votes
-    end
-    
-    it "edge case" do
-    end
-  end
+  # describe "METHOD: spotlight_winner()" do
+  #   it "nominal case" do
+  #     # need votes
+  #   end
   
-  describe "METHOD: self.all_in()" do
-    # it "nominal case" do
-    #   # arrange, already verified in "VALIDATIONS"
-    #   Work.all.each do |piece|
-    #     piece.save
-    #   end
-    
-    #   # act/assert
-    #   categories = ["movie", "book", "album"]
-    #   categories.each_with_index do |category, index|
-    #     all_in_category = Work.all_in(category: category)
-    #     if category == "movie"
-    #       assert(all_in_category.count == 11)
-    #     else
-    #       assert(all_in_category.count == 2)
-    #     end
-    
-    #     all_in_category.each do |db_piece|
-    #       assert(db_piece.category == categories[index])
-    
-    #       # this is ok b/c work titles must be unique
-    #       yml_piece = all_yml_works.select do |piece| 
-    #         (piece.title == db_piece.title) && (piece.category == db_piece.category)
-    #       end
-    
-    #       attributes = [ :title, :published_year, :creator, :category, :description]
-    #       # attributes.each do |attrib|
-    #       #   puts "LOOKING AT #{attrib}"
-    #       #   # puts yml_piece[attrib] , "VS", db_piece[attrib]
-    #       #   assert( yml_piece[attrib] == db_piece[attrib] )
-    #       # end
-    #     end
-    #   end
-    # end
-    
-    it "edge case: what if nothing is in database?" do
-      Work.destroy_all
-      categories = ["movie", "book", "album"]
-      categories.each_with_index do |category, index|
-        all_in_category = Work.all_in(category: category)
-        assert(all_in_category.count == 0)
-      end
-    end
-  end
+  #   it "edge case" do
+  #   end
+  # end
   
-  describe "METHOD: self.top_ten_in()" do
-    it "nominal case" do
-      #### need votes
-    end
-    
-    it "edge case" do
-    end
-  end
+  # describe "METHOD: self.all_in()" do
+  # it "nominal case" do
+  #   # arrange, already verified in "VALIDATIONS"
+  #   Work.all.each do |piece|
+  #     piece.save
+  #   end
+  
+  #   # act/assert
+  #   categories = ["movie", "book", "album"]
+  #   categories.each_with_index do |category, index|
+  #     all_in_category = Work.all_in(category: category)
+  #     if category == "movie"
+  #       assert(all_in_category.count == 11)
+  #     else
+  #       assert(all_in_category.count == 2)
+  #     end
+  
+  #     all_in_category.each do |db_piece|
+  #       assert(db_piece.category == categories[index])
+  
+  #       # this is ok b/c work titles must be unique
+  #       yml_piece = all_yml_works.select do |piece| 
+  #         (piece.title == db_piece.title) && (piece.category == db_piece.category)
+  #       end
+  
+  #       attributes = [ :title, :published_year, :creator, :category, :description]
+  #       # attributes.each do |attrib|
+  #       #   puts "LOOKING AT #{attrib}"
+  #       #   # puts yml_piece[attrib] , "VS", db_piece[attrib]
+  #       #   assert( yml_piece[attrib] == db_piece[attrib] )
+  #       # end
+  #     end
+  #   end
+  # end
+  
+  #   it "edge case: what if nothing is in database?" do
+  #     Work.destroy_all
+  #     categories = ["movie", "book", "album"]
+  #     categories.each_with_index do |category, index|
+  #       all_in_category = Work.all_in(category: category)
+  #       assert(all_in_category.count == 0)
+  #     end
+  #   end
+  # end
+  
+  # describe "METHOD: self.top_ten_in()" do
+  #   it "nominal case" do
+  #     #### need votes
+  #   end
+  
+  #   it "edge case" do
+  #   end
+  # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,48 +10,57 @@ class ActiveSupport::TestCase
   fixtures :all
   
   # Add more helper methods to be used by all tests here...
-  def get_db_user(yml_key:)
-    db_user = User.find_by(name: yml_key.name)
-    if db_user
-      return db_user
-    else
-      raise ArgumentError, "yml_key doesn't match anything in users.yml"
-    end
-  end
   
-  def get_db_work(yml_key:)
-    db_work = Work.find_by(title: yml_key.title)
-    if db_work
-      return db_work
-    else
-      raise ArgumentError, "yml_key doesn't match anything in works.yml"
-    end
-  end
-  
-  def get_db_vote(yml_key:)
-    db_vote = Vote.find_by(user_id: yml_key.user_id)
-    if db_vote
-      return db_vote
-    else
-      raise ArgumentError, "yml_key doesn't match anything in votes.yml"
-    end
-  end
-  
-  def let_turbo
-    # returns a big old hash for serious drying out of before 
-    album1 = works(:album1)
-    movie1 = works(:movie1)
+  def let_yml_superhash
+    # returns a big old hash for drying out of LET statements, 
+    # b/c i'm sick of specifying which yml file things come out of,
+    # maybe i could've dried this out too, but i'm only doing this one time so whatever
     return { 
-      album1: album1, 
-      movie1: movie1,
+      album1: works(:album1), 
+      album2: works(:album2),
+      book1: works(:book1),
+      book2: works(:book2),
+      movie1: works(:movie1),
+      movie2: works(:movie2),
+      movie3: works(:movie3),
+      movie4: works(:movie4),
+      movie5: works(:movie5),
+      movie6: works(:movie6),
+      movie7: works(:movie7),
+      movie8: works(:movie8),
+      movie9: works(:movie9),
+      movie10: works(:movie10),
+      movie11: works(:movie11),
+      
+      user1: users(:user1),
+      user2: users(:user2),
+      user3: users(:user3),
+      user4: users(:user4),
+      user5: users(:user5),
+      user6: users(:user6),
+      
+      vote1m1: votes(:vote1m1),
+      vote1m2: votes(:vote1m2),
+      vote1m3: votes(:vote1m3),
+      vote1m4: votes(:vote1m4),
+      vote1m5: votes(:vote1m5),
+      vote1m6: votes(:vote1m6),
+      vote1m7: votes(:vote1m7),
+      vote1m8: votes(:vote1m8),
+      vote1m9: votes(:vote1m9),
+      vote1m10: votes(:vote1m10),
+      
+      vote2m1: votes(:vote2m1),
+      vote2m2: votes(:vote2m2),
+      vote2m3: votes(:vote2m3),
+      vote3m1: votes(:vote3m1),
+      vote3m2: votes(:vote3m2),
+      vote4m1: votes(:vote4m1),
+      
+      vote1b1: votes(:vote1b1),
+      vote1b2: votes(:vote1b2),
+      vote2b2: votes(:vote2b2),
+      vote1a1: votes(:vote1a1)
     }
-  
-    # let (:album1) { works(:album1) }
-    # let (:book1) { works(:book1) }
-    # let (:movie1) { works(:movie1) }
-    # let (:album2) { works(:album2) }
-    # let (:book2) { works(:book2) }
-    # let (:movie2) { works(:movie2) }
   end
-  
 end
