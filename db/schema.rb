a34b8a5c8fa5d7da95b2252b934d94924c67de19
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-18 15:24:41 -0700

    Nullify dependent relationships on destroy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_170350) do
+ActiveRecord::Schema.define(version: 2019_10_18_221758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,4 @@ ActiveRecord::Schema.define(version: 2019_10_17_170350) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "votes", "users"
-  add_foreign_key "votes", "works"
 end
