1f5bf69eedf4ea5a9772d16c0f46913d7884e252
Mariya Burrows <mariya.burrows@gmail.com>
2019-10-18 14:22:38 -0700

    added tests for relationship between user and votes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,4 +24,21 @@ describe User do
       expect(bad_result).must_equal false
     end
   end
+  
+  describe "relationships" do 
+    before do 
+      # Arrange
+      @user = User.create(username: "hello_world")
+      @movie = Work.create(category: "movie", title: "Nightmare Before Chirstmas", creator: "Tim Burton", publication_year: 1993, description: "Jack Skellington tries to bring Christmas magic to Halloween Town. Chaos ensues.")
+      @movie2 = Work.create(category: "movie", title: "Hocus Pocus", creator: "Fox", publication_year: 1993, description: "Super spooky.")
+      @vote = Vote.create(user_id: @user.id, work_id: @movie.id)
+      @vote2 = Vote.create(user_id: @user.id, work_id: @movie2.id)
+    end
+    
+    it "can have many votes" do 
+      expect(@user.votes.length).must_equal 2
+      expect(@user.votes[0]).must_be_kind_of Vote
+    end
+    
+  end
 end
