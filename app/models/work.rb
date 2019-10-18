f0acb424d0543e5bca39e33492027f89f32e0612
Janice Huang <janicehuang@gmail.com>
2019-10-17 21:17:50 -0700

    start two helper methods in work model, sort_works and top_ten

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,17 +4,30 @@ class Work < ApplicationRecord
   validates :category, presence: true
   validates :release_date, numericality: { only_integer: true}
 
+  def self.sort_works
+    hash = {}
+    self.each do |work|
+      if work.votes.nil?
+        hash[work.id] = 0
+      else
+        hash[work.id] = work.votes.length
+      end
+      sorted_array = hash.sort_by { |key, value| -value }
+      sorted_array.map! {|subarray| Work.find(subarray[0])}
+    end
+  end
+
+  # def self.sort_works
+  #   return self.order(votes: :desc)
+  # end
+
   def self.top_ten
     works = Work.all
     if works.nil? || works.length == 0
       return ""
     end
-    hash = {}
-    works.each do |work|
-      hash[work.id] = work.votes.length
-    end
-    sorted_array = hash.sort_by { |key, value| -value }
-    sorted_array.map! {|subarray| Work.find(subarray[0])}
+
+    sorted_array = self.sort_works
 
     if sorted_array.length < 10
       return sorted_array
