e48cdc3f9d4e66b866709e1df145472c43605f5d
Rinostar <codingrinostar@gmail.com>
2019-10-17 22:59:46 -0700

    Delete Votes Columns from Users and Works tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_055358) do
+ActiveRecord::Schema.define(version: 2019_10_18_055845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 2019_10_18_055358) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.date "joined"
-    t.bigint "votes_id"
-    t.index ["votes_id"], name: "index_users_on_votes_id"
   end
 
   create_table "votes", force: :cascade do |t|
@@ -42,12 +40,8 @@ ActiveRecord::Schema.define(version: 2019_10_18_055358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "publication_year"
-    t.bigint "votes_id"
-    t.index ["votes_id"], name: "index_works_on_votes_id"
   end
 
-  add_foreign_key "users", "votes", column: "votes_id"
   add_foreign_key "votes", "users"
   add_foreign_key "votes", "works"
-  add_foreign_key "works", "votes", column: "votes_id"
 end
