85b58faa5e3e527f97b8a7b53d6d249cc38cdb3f
Brianna Kemp <kemp.bri@gmail.com>
2019-10-18 13:54:59 -0700

    Updated homepage to use controller to get work info

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,6 +1,10 @@
 class HomepagesController < ApplicationController
   def index
     @works = Work.all
+    @spotlight = Work.spotlight
+    @movies = Work.top10("movie")
+    @albums = Work.top10("album")
+    @books = Work.top10("book")
   end
   
   def show
