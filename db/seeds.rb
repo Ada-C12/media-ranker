5981d0397d4bd00edf134a9277f6b035de901a04
Vi <vanessareigosa@gmail.com>
2019-10-18 13:21:59 -0700

    changes to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# 
+
 require 'csv'
-​
-WORK_FILE = Rails.root.join('db', 'media-seeds.csv')
-puts "Loading raw work data from #{WORK_FILE}"
-​
 work_failures = []
-CSV.foreach(WORK_FILE, :headers => true) do |row|
-  work = Work.new
-  work.category = row['category']
-  work.title = row['title']
-  work.creator = row['creator']
-  work.publication_year = row['publication_year']
-  work.description = row['description']
-  successful = work.save
-  if !successful
+CSV.read(Rails.root.join('db', 'media-seeds.csv'), headers: true).each do |row|
+  t = Work.new(row.to_hash)
+  successful = t.save
+  if !successful 
     work_failures << work
-    puts "Failed to save work: #{work.inspect}"
+    puts "failed to save work : #{work.inspect}"
   else
-    puts "Created work: #{work.inspect}"
-  end
-end
-​
-puts "Added #{Work.count} work records"
-puts "#{work_failures.length} works failed to save"
-​
-puts "done"
-
-
-# require 'csv'
-# work_failures = []
-# CSV.read(Rails.root.join('db', 'media-seeds.csv'), headers: true).each do |row|
-#   t = Work.new(row.to_hash)
-#   successful = t.save
-#   if !successful 
-#     work_failures << work
-#     puts "failed to save work : #{work.inspect}"
-#   else
-#     puts "#{t.id}, #{t.title} saved"
-#   end 
-# end 
+    puts "#{t.id}, #{t.title} saved"
+  end 
+end 
