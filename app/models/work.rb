56aaea2255307a5611d8df53d6268262c8476fb4
Janice Huang <janicehuang@gmail.com>
2019-10-18 13:59:53 -0700

    get class helper method called apprioriately

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,9 +4,9 @@ class Work < ApplicationRecord
   validates :category, presence: true
   validates :release_date, numericality: { only_integer: true}
 
-  def self.sort_works
+  def self.sort_works(works)
     hash = {}
-    self.each do |work|
+    works.each do |work|
       if work.votes.nil?
         hash[work.id] = 0
       else
@@ -17,17 +17,13 @@ class Work < ApplicationRecord
     end
   end
 
-  # def self.sort_works
-  #   return self.order(votes: :desc)
-  # end
-
   def self.top_ten
     works = Work.all
     if works.nil? || works.length == 0
       return ""
     end
 
-    sorted_array = self.sort_works
+    sorted_array = sort_works(works)
 
     if sorted_array.length < 10
       return sorted_array
