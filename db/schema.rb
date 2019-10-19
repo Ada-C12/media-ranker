74ad489f24318eced5a17a85b822e5454d3d3adf
ohcloud <cloudylopez@icloud.com>
2019-10-19 14:47:37 -0700

    going to push this branch to master so that i can deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_012302) do
-  
+ActiveRecord::Schema.define(version: 2019_10_19_210525) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "work_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
-  
+
   create_table "works", force: :cascade do |t|
     t.string "category"
     t.string "title"
@@ -35,5 +39,7 @@ ActiveRecord::Schema.define(version: 2019_10_16_012302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
+  add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
