e746a7a47aa2e96c95e1cf58028ca3537988ba3d
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-18 21:13:00 -0700

    application_ctrller tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,18 @@ class ApplicationController < ActionController::Base
   
   def list_error_messages(rejected_obj)
     msgs = rejected_obj.errors.full_messages
-    if msgs
+    
+    unless msgs == []
       msgs_strs_in_array = []
       msgs.each do |msg|
         msgs_strs_in_array << "#{msg.capitalize}"
       end
       return msgs_strs_in_array
+      
     else
       raise ArgumentError, "You Don't need this method! Your #{rejected_obj}.save was successful..."
     end
+    
   end
   
 end
