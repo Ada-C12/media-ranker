c1f5bb389f22dbe4a544dcc1579c1eff5ecf51a1
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-19 12:23:37 -0700

    Votes#create halfway tested

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -21,17 +21,47 @@ describe VotesController do
     end
     
     describe "If user is LOGGED IN" do
-      it "if user never voted for this work item, should create vote with correct attribs" do
-        login(user6)
-        assert(session[:user_id] == user6.id)
+      
+      describe "if user never voted for this work, can create vote with correct attribs..." do
         
-        expect{post work_create_vote_path(work_id: movie1.id)}.must_differ "Vote.count", 1
-        db_vote = Vote.last
-        assert(db_vote.user == user6)
-        assert(db_vote.work == movie1)
+        it "... and redirected to same Works index page and see flash msg" do
+          login(user6)
+          assert(session[:user_id] == user6.id)
+          assert(user6.votes.count == 0)
+          
+          get works_path
+          assert(session[:origin_prefix] == "works")
+          
+          expect{post work_create_vote_path(work_id: movie1.id)}.must_differ "Vote.count", 1
+          db_vote = Vote.last
+          assert(db_vote.user == user6)
+          assert(db_vote.work == movie1)
+          
+          assert(flash[:success] == "Successfully upvoted!")
+          must_redirect_to works_path
+        end
+        
+        it "... and redirected to same Works/:id show page and see flash msg" do
+          login(user6)
+          assert(session[:user_id] == user6.id)
+          assert(user6.votes.count == 0)
+          
+          get work_path(id: movie1.id)
+          assert(session[:origin_prefix] == "work")
+          
+          expect{post work_create_vote_path(work_id: movie1.id)}.must_differ "Vote.count", 1
+          db_vote = Vote.last
+          assert(db_vote.user == user6)
+          assert(db_vote.work == movie1)
+          
+          assert(flash[:success] == "Successfully upvoted!")
+          must_redirect_to work_path(id: movie1.id)
+        end
+        
       end
       
       it "if user already voted for this work item, should NOT create vote" do
+        ###
       end
       
     end
