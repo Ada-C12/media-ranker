2b773aad54823e5d24ef60c2c2119532b3acb057
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-19 15:33:13 -0700

    Added tests for non-logged in user upvotes

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -28,5 +28,21 @@ describe Vote do
       refute(is_valid)
       
     end
+    
+    it "does not create a vote without a logged in user" do
+      work = Work.first
+      vote = Vote.create(user_id: nil, work_id: work.id)
+      is_valid = vote.valid?
+      
+      refute(is_valid)
+    end
+    
+    it "redirects the user with an error message if they try to vote without being logged in" do
+      work = Work.first
+      Vote.create(user_id: nil, work_id: work.id)
+      must_respond_with :redirect
+      
+      expect(flash.any?).must_equal true
+    end
   end
 end
