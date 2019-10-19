e471b5772da7750d3d124545f4373d41f02152a3
emaust <erikamaust@gmail.com>
2019-10-18 21:16:52 -0700

    Safety commit, c/p work controller instance variables to homepage controller, homepage has all elements functioning (unstyled)

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,5 +2,8 @@ class HomepageController < ApplicationController
     
     def index
         @spotlight = Work.spotlight
+        @top_ten_movies = Work.highest_ten("movie")
+        @top_ten_books = Work.highest_ten("book")
+        @top_ten_albums = Work.highest_ten("album")
     end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,23 +1,21 @@
+require 'pry'
+
 class Work < ApplicationRecord
   has_many :votes
   
   def self.spotlight
     @works = Work.all.sample
     # highest_voted = works.max_by { |work| work.votes.length }
-    
     # return highest_voted
   end
   
+  
   def self.highest_ten(category)
     works_by_category = Work.where(category: category)
-    sorted = works_by_category.sort { |i, j| i}
-    [1, 2, 3].sort { |a, b| b <=> a }
+    sorted = works_by_category.sort { |i, j|j.votes.count <=> i.votes.count }
     
-    return sorted[-1..-10].compact
+    
+    return sorted
   end
-  # get the work by the category
-  # search for the ten longest votes
-  # return that as an array to list through in the erb
-  
   
 end
