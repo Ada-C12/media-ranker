888d6768cebe68dc193f56238e86c4d35af84c5a
geli-gel <angele.zam@gmail.com>
2019-10-19 16:53:41 -0700

    added user validation test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "can be instantiated" do
+    # Arrange
+    existing_vote = votes(:user_1_top_album)
+    # Assert
+    expect(existing_vote.valid?).must_equal true
+    expect(existing_vote).must_be_instance_of Vote
+  end
+
+  describe "validations" do
+    it "cannot be saved to the database without a username" do
+      # Act
+      user_without_username = User.new()
+      expect(user_without_username.valid?).must_equal false
+    end
+  end
 end
