46b694d634a884a607c38310a8eb38e527c5234c
daniela sanchez <dnsanche@gmail.com>
2019-10-19 12:05:36 -0700

    Added that user cannot vote without logged in and more than once for same work.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,19 @@
 class VotesController < ApplicationController
 
   def create
-    user = User.find_by(id: session[:user_id])
-    @work = Work.find_by(id: params[:work_id])
-    vote = Vote.new(work_id: params[:work_id], user_id: user.id, date: Time.now)
-
-    if @work.votes.find_by(user_id: user.id) == nil 
-      vote.save
-      flash[:success] = "Successfully voted!"
-    elsif session[:user_id] == nil
-      flash.now[:failure] = "A problem occurred: You must log in to do that"
-    else 
-      flash[:failure] = "You cannot vote more than once for same work."
-    end
+    if session[:user_id] != nil  
+      user = User.find_by(id: session[:user_id])
+      @work = Work.find_by(id: params[:work_id])
+      vote = Vote.new(work_id: params[:work_id], user_id: user.id, date: Time.now)
+      if @work.votes.find_by(user_id: user.id) == nil
+        vote.save
+        flash[:success] = "Successfully voted!"
+      else 
+        flash[:failure] = "You cannot vote more than once for same work."
+      end
+    else
+      flash[:failure] = "A problem occurred: You must log in to do that"
+    end   
     redirect_to works_path
   end
-
 end
