e89bcda6a4ba4f2cbd3c5a676c522fa398780a21
ohcloud <cloudylopez@icloud.com>
2019-10-19 15:06:42 -0700

    fixed validations to deploy to heroku. was getting an error.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,4 @@
 class Vote < ApplicationRecord
   belongs_to :user, :work
-  validates :user, presence: true, uniqueness: { scope: :work } #ensures a user has to be present to vote and they can only vote once
+  validates :user_id, presence: true, uniqueness: { scope: :work_id } #ensures a user has to be present to vote and they can only vote once
 end
