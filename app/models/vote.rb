144ed355629f36749c22ff0d6fe5d57bf8aa0d3d
ohcloud <cloudylopez@icloud.com>
2019-10-19 15:10:56 -0700

    still working it out

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,5 @@
 class Vote < ApplicationRecord
-  belongs_to :user, :work
+  belongs_to :user
+  belongs_to :work
   validates :user_id, uniqueness: { scope: :work_id } #ensures a user has to be present to vote and they can only vote once
 end
