986984e6894118ed30f5f99fd1e5748d8697b2cb
ohcloud <cloudylopez@icloud.com>
2019-10-19 15:09:16 -0700

    working out the kinks with the validation to push to heroku

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,4 @@
 class Vote < ApplicationRecord
   belongs_to :user, :work
-  validates :user_id, presence: true, uniqueness: { scope: :work_id } #ensures a user has to be present to vote and they can only vote once
+  validates :user_id, uniqueness: { scope: :work_id } #ensures a user has to be present to vote and they can only vote once
 end
