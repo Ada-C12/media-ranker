f3944f9b350883ef0dd7719a29d80c48e1597628
emaust <erikamaust@gmail.com>
2019-10-20 16:27:25 -0700

    Migration for removing the date type from the publication_year column, adding column to create publication_year data type as integer. Created login routes and pages, minor layout for login page complete.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+    
+    def index
+        @users = User.all
+    end
+    
+    
+    def login_form
+        @user = User.new
+    end
+    
+    def login
+        username = params[:user][:username]
+        user = User.find_by(username: username)
+        if user
+            session[:user_id] = user.id
+            flash[:success] = "You are now logged in as #{username}"
+        else
+            user = User.create(username: username)
+            session[:user_id] = user.id
+            flash[:success] = "#{username} has been logged out"
+        end
+        
+        redirect_to root_path
+    end
+    
+    def current
+        @current_user = User.find_by(id: session[:user_id])
+        unless @current_user
+            flash[:error] = "You must log in to see this page"
+            redirect_to root_path
+        end
+    end
+    
+    def logout
+        session[:user_id] = nil
+        flash[:success] = "Grats, you're not here anymore"
+        redirect_to root_path
+    end
+    
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_215735) do
+ActiveRecord::Schema.define(version: 2019_10_20_221646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,10 +34,10 @@ ActiveRecord::Schema.define(version: 2019_10_18_215735) do
     t.string "category"
     t.string "title"
     t.string "creator"
-    t.date "publication_year"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "publication_year"
   end
 
   add_foreign_key "votes", "users"
