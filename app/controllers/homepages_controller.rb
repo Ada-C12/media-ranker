6e0e38274470efefee529a96c96cf76a87268a90
Diana <dianah46@gmail.com>
2019-10-20 12:26:18 -0700

    completed css bootstrap

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -2,8 +2,9 @@ class HomepagesController < ApplicationController
 
   def index 
     @spotlight = Work.spotlight.first
-    @albums_top_ten = Work.album_list.first(10)
+
     @books_top_ten = Work.book_list.first(10)
+    @albums_top_ten = Work.album_list.first(10)
     @movies_top_ten = Work.movie_list.first(10)
   end
 end
