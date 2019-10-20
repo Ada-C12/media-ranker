e0c002aea91558185aa95285f23d9c35d5003f71
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-20 13:53:41 -0700

    Tests are complete for all custom work model methods.

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -106,21 +106,21 @@ describe Work do
         book_1 = Work.create(category: "book", title: "The Plum Cookbook", creator: "Anna Strubinsky", publication: 1994, description: "Plum cookin'.")
         
         book_2 = Work.create(category: "book", title: "Hello Stranger", creator: "Emmylou Harris", publication: 1972, description: " ")
-
+        
         user_1 = User.create(name: "User 1")
         user_2 = User.create(name: "User 2")
         user_3 = User.create(name: "User 3")
-
+        
         Vote.create(work_id: book_1.id, user_id: user_1.id)
         Vote.create(work_id: book_1.id, user_id: user_2.id)
         Vote.create(work_id: book_2.id, user_id: user_3.id)
-
+        
         top_books = Work.top_ten("book")
         
         expect( top_books[0] ).must_equal book_1
         expect( top_books[1] ).must_equal book_2
-
-
+        
+        
       end
       
       it "doesn't add works with zero votes" do
@@ -130,34 +130,34 @@ describe Work do
         expect( Work.top_ten("album").length ).must_equal 0
         
       end
-            
+      
       it "returns a maximum of ten works" do
         book_01 = Work.create(category: "book", title: "The Plum Cookbook", creator: "Anna Strubinsky", publication: 1994, description: "Plum cookin'.")
         
         book_02 = Work.create(category: "book", title: "Hello Stranger", creator: "Emmylou Harris", publication: 1972, description: " ")
-
+        
         book_03 = Work.create(category: "book", title: "The Plum Cookbook II", creator: "Anna Strubinsky", publication: 1996, description: "More plum cookin'.")
         
         book_04 = Work.create(category: "book", title: "Hello Danger", creator: "Emmylou Harris", publication: 1974, description: " ")
-
+        
         book_05 = Work.create(category: "book", title: "The Plum Cookbook III", creator: "Anna Strubinsky", publication: 1999, description: "Even more plum cookin'.")
         
         book_06 = Work.create(category: "book", title: "Hello Manger", creator: "Emmylou Harris", publication: 1975, description: " ")
-
+        
         book_07 = Work.create(category: "book", title: "The Plum Cookbook Revisited", creator: "Anna Strubinsky", publication: 2000, description: "Plum cookin' all over again.")
         
         book_08 = Work.create(category: "book", title: "Hello Finger", creator: "Constance Horowitz", publication: 1982, description: " ")
-
+        
         book_09 = Work.create(category: "book", title: "The Plum Stranger", creator: "Anna Strubinsky", publication: 1994, description: "A stranger made of plums.")
         
         book_10 = Work.create(category: "book", title: "Goodbye Stranger", creator: "Emmylou Harris", publication: 2005, description: " ")
-
+        
         book_11 = Work.create(category: "book", title: "The Plum Resource Guide", creator: "Anna Strubinsky", publication: 2006, description: "Plum cookin' repackaged.")
         
         user_1 = User.create(name: "User 1")
         user_2 = User.create(name: "User 2")
         user_3 = User.create(name: "User 3")
-
+        
         Vote.create(work_id: book_01.id, user_id: user_1.id)
         Vote.create(work_id: book_01.id, user_id: user_2.id)
         Vote.create(work_id: book_02.id, user_id: user_3.id)
@@ -170,10 +170,49 @@ describe Work do
         Vote.create(work_id: book_09.id, user_id: user_1.id)
         Vote.create(work_id: book_10.id, user_id: user_2.id)
         Vote.create(work_id: book_11.id, user_id: user_3.id)
-
+        
         top_books = Work.top_ten("book")
         
         expect( top_books.length ).must_equal 10
+        
+      end
+      
+    end
+    
+    describe "votes_descending" do 
+      
+      it "returns all works in a category sorted from most to least votes" do
+        
+        book_1 = Work.create(category: "book", title: "The Plum Cookbook", creator: "Anna Strubinsky", publication: 1994, description: "Plum cookin'.")
+        
+        book_2 = Work.create(category: "book", title: "Hello Stranger", creator: "Emmylou Harris", publication: 1972, description: " ")
+        
+        user_1 = User.create(name: "User 1")
+        user_2 = User.create(name: "User 2")
+        user_3 = User.create(name: "User 3")
+        
+        # book_1 gets one vote
+        Vote.create(work_id: book_1.id, user_id: user_1.id)
+        
+        # book_2 gets two votes
+        Vote.create(work_id: book_2.id, user_id: user_2.id)
+        Vote.create(work_id: book_2.id, user_id: user_3.id)
+
+        books_descending = Work.votes_descending("book")
+        
+        # book_2 got the most votes so it should show up first
+        expect( books_descending[0] ).must_equal book_2
+        expect( books_descending[1] ).must_equal book_1
+        
+      end
+      
+      it "includes works that have not received any votes" do
+       
+        unloved_book = Work.create(category: "book", title: "Toenails of the World", creator: " ", publication: 1978, description: " ")
+
+        books_descending = Work.votes_descending("book")
+        
+        expect( books_descending ).must_include unloved_book
 
       end
       
