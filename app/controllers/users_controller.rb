3afef9252019c173e7a1408c08d2ed43e53be3ab
Ga-Young Jin <gyjinn@gmail.com>
2019-10-20 20:17:48 -0700

    added function for user to sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,48 +8,58 @@ class UsersController < ApplicationController
   
   def show ; end
   
-  def new
-    @user = User.new
-  end
+  # def create
+  #   @user = User.new(user_params)
   
-  def create
-    @user = User.new(user_params)
-    
-    if @user.save
-      redirect_to user_path(@user.id)
-      return
-    else 
-      render :new 
-      return
-    end
-  end
-  
-  # def edit ; end
-  
-  # def update
-  #   if @user.nil?
-  #     head :not_found
-  #     return
-  #   elsif @user.update(user_params)
+  #   if @user.save
   #     redirect_to user_path(@user.id)
   #     return
   #   else 
-  #     render :edit
+  #     render :new 
   #     return
   #   end
   # end
   
-  # def destroy
-  #   if @user.nil?
-  #     head :not_found
-  #     return
-  #   elsif @user.destroy
-  #     redirect_to users_path
-  #     return
-  #   else
-  #     render :show
-  #   end
-  # end
+  def login_form
+    @user = User.new
+  end
+  
+  def login
+    name = params[:user][:name]
+    user = User.find_by(name: name)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{name}"
+      redirect_to root_path
+      return
+    else
+      user = User.new(name: name)
+      if user.save
+        session[:user_id] = user.id
+        flash[:success] = "Successfully logged in as new user #{name}"
+        redirect_to root_path
+        return
+      else
+        flash[:failure] = "Name cannot be blank"
+        redirect_to login_path
+        return
+      end
+    end
+  end
+  
+  def current
+    @current_user = User.find_by(id: session[:user_id])
+    unless @current_user
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+    end
+  end
+  
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "You have been sucessfully logged out"
+    redirect_to root_path
+  end
   
   private
   
