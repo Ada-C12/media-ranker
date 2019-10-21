1c242c6ad3021ed30141f99dda6d847b325a56f1
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-21 01:17:04 -0700

    Changes to work tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -41,15 +41,47 @@ describe Work do
   end
 
   describe "custom methods" do
-    it "selects only ten works even when the category contains more than ten" do
-      # used Faker because the title must be unique according to Work validations
-      15.times do
-        Work.create(category: "book", title: Faker::Book.unique.title, creator: "Author")
+    describe "top ten method" do
+      it "selects only ten works even when the category contains more than ten" do
+        # used Faker because the title must be unique according to Work validations
+        15.times do
+          Work.create(category: "book", title: Faker::Book.unique.title, creator: "Author")
+        end
+
+        top_ten_books = Work.top_ten("book")
+        
+        expect(top_ten_books.length).must_equal 10
       end
 
-      top_ten_books = Work.top_ten("book")
+      it "selects all works when the category contains fewer than ten works" do
+        # used Faker because the title must be unique according to Work validations
+        5.times do
+          Work.create(category: "book", title: Faker::Book.unique.title, creator: "Author")
+        end
+
+        top_books = Work.top_ten("book")
+        
+        assert( top_books.length < 10 )
+      end
+
+      # it "returns an empty array when a category contains no works" do
+      #   # used Faker because the title must be unique according to Work validations
+
+      #   top_books = Work.top_ten("book")
+
+      #   p top_books
+        
+      #   expect(top_books.length).must_equal 0
+      # end
+    end
+    describe "spotlight method" do
+      before do
+        Vote.destroy_all
+      end
       
-      expect(top_ten_books.length).must_equal 10
+      it "displays a random work if no works have votes" do
+        
+      end
     end
   end
 end
