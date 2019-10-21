1b7661a0933f039ecc7fed2977a92b330252f65e
Brianna Kemp <kemp.bri@gmail.com>
2019-10-20 20:04:30 -0700

    Updated flash button naming for formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,13 +24,13 @@ class UsersController < ApplicationController
     
     if found_user
       session[:user_id] = found_user.id
-      flash[:message] = "Logged in as returning user!"
+      flash[:login] = "Logged in as returning user!"
     else
       new_user = User.new(username: username)
       new_user.save
       # TODO: What happens if saving fails?
       session[:user_id] = new_user.id
-      flash[:message] = "Successfully created new user #{new_user.username} with ID #{new_user.id}"
+      flash[:login] = "Successfully created new user #{new_user.username} with ID #{new_user.id}"
     end
     
     return redirect_to root_path
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   def logout
     # TODO: What happens if we were never logged in?
     session[:user_id] = nil
-    flash[:message] = "You have logged out successfully."
+    flash[:logout] = "You have logged out successfully."
     return redirect_to root_path
   end
   
