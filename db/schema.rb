457e2986fa74b4ab9954347a18eb5f21455c106c
Brianna Kemp <kemp.bri@gmail.com>
2019-10-20 20:02:35 -0700

    Removed extra column from votes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_185841) do
+ActiveRecord::Schema.define(version: 2019_10_21_025801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2019_10_17_185841) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
