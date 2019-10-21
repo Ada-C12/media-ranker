62aa7247459c006085bb9b0ca278fe30af1bc042
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-20 22:53:11 -0700

    completed wave 3 display requirements, updated frontpage view for spotlight to include vote count and description

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,7 @@ class Vote < ApplicationRecord
   belongs_to :user
   
   validates :user_id, presence: true
-  validates :user_id, uniqueness: { scope: :work }
+  validates :user_id, uniqueness: { scope: :work, message: "has already voted for this work" }
   validates :work_id, presence: true
 
 end
