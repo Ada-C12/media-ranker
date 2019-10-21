e2c708ac9b3da6a8d3446faa630b3b494f30834f
geli-gel <angele.zam@gmail.com>
2019-10-20 18:47:03 -0700

    made partial views for login/logout buttons dependent on if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  before_action :determine_user
+
+
+  private
+
+  def determine_user 
+    @determine_user = User.find_by(id: session[:user_id])
+  end
+
 end
