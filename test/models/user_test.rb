1dd5bc17041adfc32de2ed4343348603e53b3683
Alice <alicesunhi@gmail.com>
2019-11-06 10:02:12 -0800

    a

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,7 +11,7 @@ describe User do
     end
     
     it 'is invalid without a username' do
-      @user.username = nil
+      @user.username = "hi"
       
       expect(@user.valid?).must_equal false
       expect(@user.errors.messages).must_include :username
